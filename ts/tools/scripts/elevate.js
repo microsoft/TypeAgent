6fcae450598152f9462b3b897b8f6fb105803f8e
Robert Gruen <robgruen@microsoft.com>
2024-08-12 19:17:12 -0700

    ran prettier

--- ts/tools/scripts/elevate.js
+++ ts/tools/scripts/elevate.js
@@ -11,15 +11,15 @@ async function elevate() {
     const client = await getClient();
 
     // elevate
-    await config.forEach(element => {
+    await config.forEach((element) => {
         var v = client.elevate({
             requestType: element.properties.RequestType,
             roleName: element.roleName,
             expirationType: element.properties.ScheduleInfo.Expiration.Type,
-            expirationDuration: element.properties.ScheduleInfo.Expiration.Duration,
-        });    
+            expirationDuration:
+                element.properties.ScheduleInfo.Expiration.Duration,
+        });
     });
-    
 }
 
 (async () => {
