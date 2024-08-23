90fae647353dfebeb95b88f128199783ef65519a
Umesh Madan <umeshma@microsoft.com>
2024-08-23 15:31:19 -0700

    Copyright

--- ts/packages/knowledgeProcessor/src/modelCache.ts
+++ ts/packages/knowledgeProcessor/src/modelCache.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { TextEmbeddingModel } from "aiclient";
 import { collections } from "typeagent";
 import { Result, success } from "typechat";
