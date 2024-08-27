2a7c8f762e53e7c937651ffe51fed1cea3b85c9a
Curtis Man <curtism@microsoft.com>
2024-08-27 09:53:46 -0700

    Fix readMessage repeating same mesage if chunk terminated with full message delimiter

---
+++ ts/packages/aiclient/test/serverEvents.spec.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { readMessages } from "../src/serverEvents";
+
+const messages = ["data: hello", "data: world", "data: foo", "data: bar"];
+
+function getTextStream(text: string, breakAt: number) {
+    return (async function* () {
+        yield text.slice(0, breakAt);
+        yield text.slice(breakAt);
+    })();
+}
+describe("serverEvents", () => {
+    describe("readMessage", () => {
+        const fullMessage = messages.join("\n\n");
+        for (let i = 0; i < fullMessage.length; i++) {
+            it(`should read messages break at ${i}`, async () => {
+                const textStream = getTextStream(fullMessage, i);
+                let index = 0;
+                for await (const message of readMessages(textStream)) {
+                    expect(message).toEqual(messages[index++]);
+                }
+            });
+        }
+    });
+});
