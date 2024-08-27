5ce9a22a60feaf506343d76b347f2408df425503
steveluc <steveluc@users.noreply.github.com>
2024-08-27 07:39:24 -0700

    add conversation lookup to chat response schema; add knowledge-processor to dispatcher and chat agent

--- ts/packages/agents/chat/src/chatResponseActionSchema.ts
+++ ts/packages/agents/chat/src/chatResponseActionSchema.ts
@@ -31,19 +31,58 @@ export interface GenerateResponseAction {
     };
 }
 
-// if the request is for contemporary chat information including sports scores, news events, or current commerce offerings, use the lookups parameter to request a lookup of the information on the user's behalf; the assistant will generate a response based on the lookup results
+export type DateVal = {
+    day: number;
+    month: number;
+    year: number;
+};
+
+export type TimeVal = {
+    // In 24 hour form
+    hour: number;
+    minute: number;
+    seconds: number;
+};
+
+export type DateTime = {
+    date: DateVal;
+    time?: TimeVal | undefined;
+};
+
+export type DateTimeRange = {
+    startDate: DateTime;
+    stopDate?: DateTime | undefined;
+};
+
+export type TermFilter = {
+    // action verb terms to look for
+    verbs?: string[];
+    // Terms are one of the following:
+    // Entity Terms:
+    // - the name of an entity or thing such as "Bach", "Great Gatsby", "frog" or "piano"
+    // - the *type* of the entity such as "speaker", "person", "artist", "animal", "object", "instrument", "school", "room", "museum", "food" etc.
+    //   An entity can have multiple types; entity types should be single words
+    // - facets: specific, inherent, defining, or non-immediate facet of an entity such as "blue", "old", "famous", "sister", "aunt_of", "weight: 4 kg"
+    // Action Terms:
+    // - subject, object and indirectObject associated with the verb
+    // verbs are not duplicated
+    terms: string[];
+    // Use only if request explicitly asks for time range
+    timeRange?: DateTimeRange | undefined; // in this time range
+};
 
 export interface LookupAndGenerateResponseAction {
     actionName: "lookupAndGenerateResponse";
     parameters: {
         // the original request from the user
         originalRequest: string;
-        // ALL the actions and entities present in the user's request
-        userRequestEntities: Entity[];
+        // if the request is for information from past conversations use the conversation lookup filters
+        conversationLookupFilters?: TermFilter[];
+        // if the request is for contemporary internet information including sports scores, news events, or current commerce offerings, use the lookups parameter to request a lookup of the information on the user's behalf; the assistant will generate a response based on the lookup results
         // Lookup *facts* you don't know or if your facts are out of date.
         // E.g. stock prices, time sensitive data, etc
         // the search strings to look up on the user's behalf should be specific enough to return the correct information
         // it is recommended to include the same entities as in the user request
-        lookups: string[];
+        internetLookups: string[];
     };
 }
