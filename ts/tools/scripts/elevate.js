e0218cb4c2f2a93e7fae0b197c7a944c9b6f6044
Robert Gruen <robgruen@microsoft.com>
2024-09-23 20:22:51 -0700

    fixed datetime parsing

--- ts/tools/scripts/elevate.js
+++ ts/tools/scripts/elevate.js
@@ -34,24 +34,22 @@ async function elevate(startDateTime) {
             console.log("Uses configuration from elevate.config.json.");
             return;
         default:
-            const start = undefined;
-
-            const date = new Date();
+            let date = new Date();
             if (command == "tomorrow_morning") {
                 date.setDate(date.getDate() + 1);
                 date.setHours(8, 0, 0);
             } else {
                 try {
-                    date = Date.parse(start);
+                    date = new Date(Date.parse(command));
                 } catch {
                     console.log(
                         `Unable to parse date '${command}'.  The expected format is '2024-09-30T09:01:00'`,
                     );
-                    exit(-2);
+                    return;
                 }
             }
 
-            await elevate(date);
+            await elevate(date.toISOString());
             break;
     }
 })().catch((e) => {
