99e48e4721d507ffa5f99c23d7b932a81a19b44e
Umesh Madan <umeshma@microsoft.com>
2024-09-26 12:54:09 -0700

    copyright

--- ts/packages/knowledgeProcessor/test/store.spec.ts
+++ ts/packages/knowledgeProcessor/test/store.spec.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { createFileNameGenerator, generateTimestampString } from "typeagent";
 
 describe("Storage", () => {
