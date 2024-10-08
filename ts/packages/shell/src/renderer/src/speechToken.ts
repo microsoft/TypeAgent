915324d7da5428d88ba0163f388edd2d133342fc
Robert Gruen <robgruen@microsoft.com>
2024-10-08 13:45:17 -0700

    working on getting shell running in the browser

--- ts/packages/shell/src/renderer/src/speechToken.ts
+++ ts/packages/shell/src/renderer/src/speechToken.ts
@@ -7,8 +7,10 @@ import { getClientAPI } from "./main";
 // globally share a token cache
 let speechToken: SpeechToken | undefined;
 export async function getSpeechToken(): Promise<SpeechToken | undefined> {
-    if (speechToken === undefined || speechToken.expire <= Date.now()) {
-        speechToken = await getClientAPI().getSpeechToken();
+    if (getClientAPI() != undefined) {
+        if (speechToken === undefined || speechToken.expire <= Date.now()) {
+            speechToken = await getClientAPI().getSpeechToken();
+        }
     }
     return speechToken;
 }
