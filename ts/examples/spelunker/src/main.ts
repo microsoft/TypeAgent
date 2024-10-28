e63dead0bb500ab77a8f408fac4d625cd8c35c6f
Guido van Rossum <gvanrossum@microsoft.com>
2024-10-28 09:22:48 -0700

    Fix lint

--- ts/examples/spelunker/src/main.ts
+++ ts/examples/spelunker/src/main.ts
@@ -13,7 +13,7 @@ import {
 import { importPythonFile } from "./pythonImporter.js";
 import { Chunk } from "./pythonChunker.js";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- ts/examples/spelunker/src/pythonChunker.ts
+++ ts/examples/spelunker/src/pythonChunker.ts
@@ -7,7 +7,7 @@
 import { exec } from "child_process";
 import path from "path";
 import { promisify } from "util";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
