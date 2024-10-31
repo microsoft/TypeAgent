d15762922e03a5e04ab0bd90d21afffc690d958f
Curtis Man <curtism@microsoft.com>
2024-10-31 09:18:14 -0700

    Explainer options

--- ts/packages/cache/src/utils/language.ts
+++ ts/packages/cache/src/utils/language.ts
@@ -6,49 +6,46 @@ export interface LanguageTools {
 }
 
 const referenceWords = [
-    "he",
-    "she",
-    "it",
-    "they",
     "him",
-    "her",
+    // "her",
+    "it",
     "them",
-    "his",
+    // "his",  // doubled by referenceParts
     "hers",
     "theirs",
+    // "its", // doubled by referenceParts
+];
+
+const referenceOfSuffixes = referenceWords.map(
+    (w) => new RegExp(`\\bof\\s${w}$`),
+);
+
+const referenceSuffixes = ["here", "there"].map((w) => new RegExp(`\\b${w}$`));
+
+const referenceParts = [
+    "his",
+    "her",
     "its",
+    "their",
     "this",
     "that",
     "these",
     "those",
-    "here",
-    "there",
-];
-
-const referencePrefixes = [
-    "his ",
-    "her ",
-    "its",
-    "their ",
-    "this ",
-    "that ",
-    "these ",
-    "those ",
-];
+].map((w) => new RegExp(`\\b${w}\\b`));
 
 // REVIEW: Heuristics to allow time references from now.
 const relativeToNow =
-    /^this (week|month|year|quarter|season|day|hour|minute|second|morning|afternoon)$/;
+    /this (week|month|year|quarter|season|day|hour|minute|second|morning|afternoon)$/;
 
 const languageToolsEn: LanguageTools = {
     possibleReferentialPhrase(phrase: string) {
         // TODO: initiali implemention. Can be overbroad and incomplete.
         const lowerCase = phrase.toLowerCase();
         return (
-            (referenceWords.includes(lowerCase) ||
-                referencePrefixes.some((prefix) =>
-                    lowerCase.startsWith(prefix),
-                )) &&
+            (referenceWords.some((r) => r === lowerCase) ||
+                referenceSuffixes.some((r) => r.test(lowerCase)) ||
+                referenceOfSuffixes.some((r) => r.test(lowerCase)) ||
+                referenceParts.some((r) => r.test(lowerCase))) &&
             !relativeToNow.test(phrase)
         );
     },
