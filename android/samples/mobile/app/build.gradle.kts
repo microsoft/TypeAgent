92b3b546a2353e48114b3c84bd4133352861c2b7
Robert Gruen <robgruen@microsoft.com>
2024-11-01 16:28:17 -0700

    fixed some code warnings

--- android/samples/mobile/app/build.gradle.kts
+++ android/samples/mobile/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.microsoft.typeagent.sample"
         minSdk = 33
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
