e07eed4b67260ac528a28f61b5b7a00b5ed3678f
hlucco <henry_lucco@brown.edu>
2024-11-07 00:27:25 -0800

    data reader

---
+++ python/fineTuning/chaparral/models/data.py
@@ -0,0 +1,59 @@
+# Copyright (c) Microsoft Corporation and Henry Lucco.
+# Licensed under the MIT License.
+
+from dataclasses import dataclass
+from chaparral.models.knowledge import KnowledgeResponse
+
+@dataclass
+class InfoPair:
+    message: str
+    knowledge: KnowledgeResponse
+
+    @classmethod
+    def from_dict(cls, data: dict):
+
+        message = data.get("message", None)
+        knowledge = data.get("knowledge", None)
+
+        if not message:
+            raise ValueError("Invalid info pair: missing message")
+        
+        if not knowledge:
+            raise ValueError("Invalid info pair: missing knowledge")
+        
+        knowledge = KnowledgeResponse.from_dict(knowledge)
+
+        return cls(message, knowledge)
+    
+    def to_dict(self):
+        return {
+            "message": self.message,
+            "knowledge": self.knowledge.to_dict()
+        }
+
+@dataclass
+class Dataset:
+    prompt: str
+    info_pairs: list[InfoPair]
+
+    @classmethod
+    def from_dict(cls, data: dict):
+
+        prompt = data.get("prompt", None)
+        info_pairs = data.get("infoPairs", None)
+
+        if not prompt:
+            raise ValueError("Invalid dataset: missing prompt")
+        
+        if not info_pairs:
+            raise ValueError("Invalid dataset: missing info pairs")
+        
+        info_pairs = [InfoPair.from_dict(pair) for pair in info_pairs]
+
+        return cls(prompt, info_pairs)
+    
+    def to_dict(self):
+        return {
+            "prompt": self.prompt,
+            "infoPairs": [pair.to_dict() for pair in self.info_pairs]
+        }
