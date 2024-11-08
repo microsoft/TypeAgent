b3e43e8e961d097498dc512869e94945dd7ca617
Umesh Madan <umeshma@microsoft.com>
2024-11-07 22:54:09 -0800

    Work queue

--- ts/packages/typeagent/src/storage/workQueue.ts
+++ ts/packages/typeagent/src/storage/workQueue.ts
@@ -3,34 +3,83 @@
 
 import path from "path";
 import fs from "fs";
-import { ensureDir, Path } from "../objStream";
+import {
+    ensureDir,
+    Path,
+    readJsonFile,
+    removeDir,
+    removeFile,
+    writeJsonFile,
+} from "../objStream";
 import { slices } from "../lib/array";
 import { asyncArray } from "..";
+import {
+    createFileNameGenerator,
+    generateTimestampString,
+} from "./objectFolder";
 
 export interface WorkQueue {
-    run(
+    count(): Promise<number>;
+    addTask(obj: any): Promise<void>;
+    drain(
         batchSize: number,
         concurrency: number,
         processor: (item: Path, index: number, total: number) => Promise<void>,
     ): Promise<void>;
+    drainTask(
+        batchSize: number,
+        concurrency: number,
+        processor: (task: any, index: number, total: number) => Promise<void>,
+    ): Promise<void>;
+    replay(): Promise<void>;
 }
 
 export async function createWorkQueueFolder(
     rootPath: string,
     queueFolderName?: string,
 ): Promise<WorkQueue> {
-    const queuePath = await ensureDir(
+    let queuePath = await ensureDir(
         path.join(rootPath, (queueFolderName ??= "queue")),
     );
-    const completedPath = await ensureDir(path.join(rootPath, "completed"));
-    const skippedPath = await ensureDir(path.join(rootPath, "skipped"));
-    const errorPath = await ensureDir(path.join(rootPath, "error"));
-
+    let completedPath = await ensureDir(path.join(rootPath, "completed"));
+    let skippedPath = await ensureDir(path.join(rootPath, "skipped"));
+    let errorPath = await ensureDir(path.join(rootPath, "error"));
+    const namedGenerator = createFileNameGenerator(
+        generateTimestampString,
+        (name: string) => {
+            return !fs.existsSync(taskFilePath(name));
+        },
+    );
     return {
-        run,
+        count,
+        addTask,
+        drainTask,
+        drain,
+        replay,
     };
 
-    async function run(
+    async function count(): Promise<number> {
+        const fileNames = await fs.promises.readdir(queuePath);
+        return fileNames.length;
+    }
+
+    async function addTask(task: any): Promise<void> {
+        const fileName = namedGenerator.next().value;
+        await writeJsonFile(taskFilePath(fileName), task);
+    }
+
+    async function drainTask(
+        batchSize: number,
+        concurrency: number,
+        processor: (task: any, index: number, total: number) => Promise<void>,
+    ) {
+        return drain(batchSize, concurrency, async (item, index, total) => {
+            const task = await readJsonFile(item);
+            processor(task, index, total);
+        });
+    }
+
+    async function drain(
         batchSize: number,
         concurrency: number,
         processor: (item: Path, index: number, total: number) => Promise<void>,
@@ -40,36 +89,49 @@ export async function createWorkQueueFolder(
         let startAt = 0;
         for (let slice of slices(fileNames, batchSize)) {
             startAt = slice.startAt;
-            try {
-                await asyncArray.forEachAsync(
-                    slice.value,
-                    concurrency,
-                    processFile,
-                );
-            } catch (err) {}
+            await asyncArray.forEachAsync(
+                slice.value,
+                concurrency,
+                processFile,
+            );
         }
 
         async function processFile(fileName: string, index: number) {
-            const filePath = path.join(queuePath, fileName);
-            const completedFilePath = path.join(completedPath, fileName);
+            const filePath = taskFilePath(fileName);
             try {
-                if (fs.existsSync(completedFilePath)) {
-                    await moveFileAsync(
-                        filePath,
-                        path.join(skippedPath, fileName),
-                    );
+                if (isAlreadyProcessed(fileName)) {
+                    await moveFileAsync(fileName, skippedPath);
                 } else {
                     await processor(filePath, startAt + index, total);
-                    await moveFileAsync(filePath, completedFilePath);
+                    await moveFileAsync(fileName, completedPath);
                 }
                 return;
             } catch {}
             // Move to the error folder
-            await moveFileAsync(filePath, path.join(errorPath, fileName));
+            await moveFileAsync(fileName, errorPath);
         }
     }
 
-    function moveFileAsync(filePath: string, targetDirPath: string) {
-        return fs.promises.rename(filePath, targetDirPath);
+    async function replay(): Promise<void> {
+        await removeDir(queuePath);
+        await fs.promises.rename(completedPath, queuePath);
+        await ensureDir(completedPath);
+    }
+
+    function isAlreadyProcessed(fileName: string) {
+        return fs.existsSync(path.join(completedPath, fileName));
+    }
+
+    async function moveFileAsync(
+        fileName: string,
+        targetDirPath: string,
+    ): Promise<void> {
+        const targetFilePath = path.join(targetDirPath, fileName);
+        await removeFile(targetFilePath);
+        await fs.promises.rename(taskFilePath(fileName), targetFilePath);
+    }
+
+    function taskFilePath(name: string): string {
+        return path.join(queuePath, name);
     }
 }
