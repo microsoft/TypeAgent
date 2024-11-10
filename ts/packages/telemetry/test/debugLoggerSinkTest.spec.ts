5263b3180e5f16d17aecdc00723fc9eb14d73930
Robert Gruen <robgruen@microsoft.com>
2024-11-09 20:41:48 -0800

    added test

--- ts/packages/telemetry/test/debugLoggerSinkTest.spec.ts
+++ ts/packages/telemetry/test/debugLoggerSinkTest.spec.ts
@@ -1,2 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
+import { createDebugLoggerSink } from "../src/indexNode.js";
+
+
+describe("Debug logger sink", () => {
+
+    it("createDebugLoggerSink should succeed", () => {
+        const sink = createDebugLoggerSink();
+
+        expect(sink).not.toBeNull();        
+    });
+});
