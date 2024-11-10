a809a9c91a7939e14feba0f902e1b807429714b5
Guido van Rossum <gvanrossum@gmail.com>
2024-11-10 15:34:21 -0800

    [spelunker] Ask the model for keywords, topics, goals and dependencies (#364)
    This is only a halfway point on the journey; querying is currently a
    mess:
    It just looks up the query as a key in each of the tables and reports
    any hits it gets.
    No stemming, no trying sub-sequences, it is even case-sensitive, etc.
    We may also need to clarify to the model what each of these means.
    
    ---------
    
    Co-authored-by: Guido van Rossum <gvanrossum@microsoft.com>

--- ts/examples/spelunker/src/chunkDocSchema.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-// Documentation for a chunk of code
-export type ChunkDocumentation = {
-    description: string; // Concise description of a chunk of code
-};
---
+++ ts/examples/spelunker/src/chunkyIndex.ts
@@ -0,0 +1,81 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { openai, ChatModel } from "aiclient";
+import {
+    CodeDocumenter,
+    createSemanticCodeIndex,
+    SemanticCodeIndex,
+} from "code-processor";
+import * as knowLib from "knowledge-processor";
+import { createObjectFolder, ObjectFolder } from "typeagent";
+
+import { CodeDocumentation } from "./codeDocSchema.js";
+import {
+    createFakeCodeDocumenter,
+    createFileDocumenter,
+    FileDocumenter,
+} from "./fileDocumenter.js";
+import { Chunk } from "./pythonChunker.js";
+
+// A bundle of object stores and indices etc.
+export class ChunkyIndex {
+    rootDir: string;
+    chatModel: ChatModel;
+    fileDocumenter: FileDocumenter;
+    fakeCodeDocumenter: CodeDocumenter;
+    // The rest are asynchronously initialized by initialize().
+    chunkFolder!: ObjectFolder<Chunk>;
+    codeIndex!: SemanticCodeIndex;
+    summaryFolder!: ObjectFolder<CodeDocumentation>;
+    childrenFolder!: knowLib.KeyValueIndex<string, string>;
+    parentFolder!: knowLib.KeyValueIndex<string, string>;
+    keywordsFolder!: knowLib.KeyValueIndex<string, string>;
+    topicsFolder!: knowLib.KeyValueIndex<string, string>;
+    goalsFolder!: knowLib.KeyValueIndex<string, string>;
+    dependenciesFolder!: knowLib.KeyValueIndex<string, string>;
+
+    private constructor(rootDir: string) {
+        this.rootDir = rootDir;
+        this.chatModel = openai.createChatModelDefault("spelunkerChat");
+        this.fileDocumenter = createFileDocumenter(this.chatModel);
+        this.fakeCodeDocumenter = createFakeCodeDocumenter();
+    }
+
+    static async createInstance(rootDir: string): Promise<ChunkyIndex> {
+        const instance = new ChunkyIndex(rootDir);
+        instance.chunkFolder = await createObjectFolder<Chunk>(
+            instance.rootDir + "/chunks",
+            { serializer: (obj) => JSON.stringify(obj, null, 2) },
+        );
+        instance.codeIndex = await createSemanticCodeIndex(
+            instance.rootDir + "/index",
+            instance.fakeCodeDocumenter,
+            undefined,
+            (obj) => JSON.stringify(obj, null, 2),
+        );
+        instance.summaryFolder = await createObjectFolder<CodeDocumentation>(
+            instance.rootDir + "/summaries",
+            { serializer: (obj) => JSON.stringify(obj, null, 2) },
+        );
+        instance.childrenFolder = await knowLib.createIndexFolder<string>(
+            instance.rootDir + "/children",
+        );
+        instance.parentFolder = await knowLib.createIndexFolder<string>(
+            instance.rootDir + "/parent",
+        );
+        instance.keywordsFolder = await knowLib.createIndexFolder<string>(
+            instance.rootDir + "/keywords",
+        );
+        instance.topicsFolder = await knowLib.createIndexFolder<string>(
+            instance.rootDir + "/topics",
+        );
+        instance.goalsFolder = await knowLib.createIndexFolder<string>(
+            instance.rootDir + "/goals",
+        );
+        instance.dependenciesFolder = await knowLib.createIndexFolder<string>(
+            instance.rootDir + "/dependencies",
+        );
+        return instance;
+    }
+}
