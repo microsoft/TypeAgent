98e8d7b9993c440c84f742b45623c22be6f7150b
Piali Choudhury <pialic@microsoft.com>
2024-11-11 11:23:29 -0800

    update readme

--- ts/examples/vscodeSchemaGen/src/genStats.ts
+++ ts/examples/vscodeSchemaGen/src/genStats.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import * as fs from "fs";
 import chalk from "chalk";
 import { similarity, SimilarityType } from "typeagent";
