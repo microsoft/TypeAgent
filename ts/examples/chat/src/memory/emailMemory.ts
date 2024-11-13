bd44299371f6815e5ab2e37814b4d324064b00fd
Umesh Madan <umeshma@microsoft.com>
2024-11-12 22:59:30 -0800

    stats

--- ts/examples/chat/src/memory/emailMemory.ts
+++ ts/examples/chat/src/memory/emailMemory.ts
@@ -86,6 +86,7 @@ export function createEmailCommands(
 ): void {
     commands.importEmail = importEmail;
     commands.convertMsg = convertMsgs;
+    commands.emailStats = emailStats;
 
     //--------
     // Commands
@@ -151,6 +152,15 @@ export function createEmailCommands(
             );
             await convertMsgFiles(sourcePath, io);
         }
+    }
+
+    commands.emailStats.metadata = "Email statistics";
+    async function emailStats(args: string[]): Promise<void> {
+        const stats = await loadStats(false);
+        context.printer.writeBullet(`Email count: ${stats.itemStats.length}`);
+        context.printer.writeBullet(
+            `Total chars: ${stats.totalStats.charCount}`,
+        );
     }
 
     //-------------
