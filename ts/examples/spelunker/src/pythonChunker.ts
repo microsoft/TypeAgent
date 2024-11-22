cc1e490174999e20aa8d673c7649a48bb39e5102
Guido van Rossum <gvanrossum@microsoft.com>
2024-11-21 18:59:27 -0800

    Split long files into pieces (simple-minded version)

--- ts/examples/spelunker/src/pythonChunker.ts
+++ ts/examples/spelunker/src/pythonChunker.ts
@@ -31,8 +31,8 @@ export interface Chunk {
     blobs: Blob[];
     parentId: ChunkId;
     children: ChunkId[];
-    fileName: string; // Set on receiving end to reduce JSON size.
-    docs?: FileDocumentation; // Computed on receiving end from file docs.
+    fileName: string; // Set upon receiving end from ChunkedFile.fileName.
+    docs?: FileDocumentation; // Computed later by fileDocumenter.
 }
 
 export interface ChunkedFile {
@@ -85,6 +85,78 @@ export async function chunkifyPythonFiles(
             for (const chunk of result.chunks) {
                 chunk.fileName = result.fileName;
             }
+        }
+    }
+    return splitLargeFiles(results);
+}
+
+function chunkSize(chunk: Chunk): number {
+    let totalCharacters = 0;
+    for (const blob of chunk.blobs) {
+        if (!blob.breadcrumb) {
+            for (const line of blob.lines) {
+                totalCharacters += line.length;
+            }
+        }
+    }
+    return totalCharacters;
+}
+
+function fileSize(file: ChunkedFile): number {
+    return file.chunks.reduce((acc, chunk) => acc + chunkSize(chunk), 0);
+}
+
+const FILE_SIZE_LIMIT = 50000; // ~50 Kbytes; assume 128K token limit.
+const CHUNK_COUNT_LIMIT = 30; // Max 30 chunks regardless of size.
+
+// First attempt. Split eagerly, without regard to dependencies.
+
+function splitLargeFiles(
+    results: (ChunkedFile | ErrorItem)[],
+): (ChunkedFile | ErrorItem)[] {
+    const largeFiles = results.filter(
+        (result): result is ChunkedFile =>
+            !("error" in result) && fileSize(result) > FILE_SIZE_LIMIT,
+    );
+    results = results.filter(
+        (result) => "error" in result || fileSize(result) <= FILE_SIZE_LIMIT,
+    );
+    for (const file of largeFiles) {
+        const fileName = file.fileName;
+        const chunkMap: Map<ChunkId, Chunk> = new Map(
+            file.chunks.map((chunk) => [chunk.id, chunk]),
+        );
+        let chunks = Array.from(file.chunks); // A copy to mess around with.
+        outer: while (true) {
+            let totalSize = 0;
+            for (let i = 0; i < chunks.length; i++) {
+                const currentChunk = chunks[i];
+                const size = chunkSize(currentChunk);
+                if (
+                    (totalSize && totalSize + size > FILE_SIZE_LIMIT) ||
+                    i >= CHUNK_COUNT_LIMIT
+                ) {
+                    const prefix: Chunk[] = chunks.slice(0, i);
+                    results.push({ fileName: file.fileName, chunks: prefix });
+                    totalSize = 0;
+                    // Now remove the chunks we just pushed, except for ancestors of the current chunk.
+                    const ancestors: Chunk[] = [];
+                    for (
+                        let c: Chunk | undefined = currentChunk;
+                        c;
+                        c = chunkMap.get(c.parentId)
+                    ) {
+                        ancestors.unshift(c);
+                    }
+                    if (ancestors.length >= i)
+                        throw new Error("Splitting is not making progress");
+                    chunks = [...ancestors, ...chunks.slice(i)];
+                    continue outer;
+                }
+            }
+            // We get here when we exhausted todo without hitting the size limit.
+            results.push({ fileName, chunks });
+            break;
         }
     }
     return results;
