4d2edfa2f566ef9fe1ce40c5a98dbae4c2c8def1
Robert Gruen <robgruen@microsoft.com>
2024-11-29 21:59:14 -0800

    redact keys and secrets form @env command.

--- ts/packages/dispatcher/src/handlers/envCommandHnadler.ts
+++ ts/packages/dispatcher/src/handlers/envCommandHnadler.ts
@@ -18,7 +18,11 @@ export class EnvCommandHandler implements CommandHandlerNoParams {
 
         for(let i = 0; i < keys.length; i++) {
             if (values[i] !== undefined) {
-                table.push([ keys[i], values[i]! ]);
+                if ((keys[i].toLowerCase().indexOf("key") > -1 && values[i]?.toLowerCase() != "identity") || keys[i].toLowerCase().indexOf("secret") > -1) {
+                    table.push([ keys[i], "__redacted__" ]);
+                } else {
+                    table.push([ keys[i], values[i]!]);
+                }            
             } else {
                 table.push([ keys[i], ""]);
             }
