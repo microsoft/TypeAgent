1dfd0c7a814d7759a907c46a792dc451ba8a9f19
Guido van Rossum <gvanrossum@microsoft.com>
2024-12-12 16:38:46 -0800

    Add comments to schema (does this use TypeChat?)

--- ts/packages/agents/oracle/src/oracleSchema.ts
+++ ts/packages/agents/oracle/src/oracleSchema.ts
@@ -3,9 +3,11 @@
 
 export type OracleAction = QueryAction;
 
+// Ask the oracle a question; it will give an oracular, pseudo-philosophical answer.
+// Good for questions like "what is the meaning of life" or "does he love me". :-)
 export type QueryAction = {
     actionName: "queryOracle";
     parameters: {
-        query: string;
+        query: string; // The question for the oracle
     };
 };
