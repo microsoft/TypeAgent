4c37f43d5bbfc8ec10f28dd0937397acaa4b97e3
Robert Gruen <robgruen@microsoft.com>
2024-12-23 11:53:36 -0800

    more updates

--- ts/examples/chat/src/memory/imageMemory.ts
+++ ts/examples/chat/src/memory/imageMemory.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { ArgDef, CommandHandler, CommandMetadata, InteractiveIo, parseNamedArguments } from "interactive-app";
+import { ArgDef, CommandHandler, CommandMetadata, InteractiveIo, NamedArgs, parseNamedArguments } from "interactive-app";
 import { ChatContext } from "./chatMemory.js";
 import { argSourceFileOrFolder } from "./common.js";
 import { isDirectoryPath } from "typeagent";
 import fs from "node:fs";
-import * as knowLib from "knowledge-processor";
+//import * as knowLib from "knowledge-processor";
 
 export function argPause(defaultValue = 0): ArgDef {
     return {
@@ -40,21 +40,25 @@ export function createImageCommands(context: ChatContext, commands: Record<strin
         if (isDir) {
             await indexImages(namedArgs, sourcePath);
         } else if (fs.existsSync(sourcePath)) {
-            if (
-                !(await knowLib.image.addImageToConversation(
-                    context.emailMemory,
-                    sourcePath,
-                    namedArgs.chunkSize,
-                ))
-            ) {
-                context.printer.writeLine(`Could not load ${sourcePath}`);
-            }
+            // if (
+            //     !(await knowLib.image.addImageToConversation(
+            //         context.emailMemory,
+            //         sourcePath,
+            //         namedArgs.chunkSize,
+            //     ))
+            // ) {
+            //     context.printer.writeLine(`Could not load ${sourcePath}`);
+            // }
         } else {
             context.printer.writeLine(`Could not find part of the file path '${sourcePath}'`);
         }
     }
 
     async function indexImages(namesArgs: NamedArgs, sourcePath: string) {
+        // load files from directory
+        const fileNames = await fs.promises.readdir(sourcePath, { recursive: true });
         
+        // index each image
+        fileNames.map((fileName) => { console.log(fileName)});
     }
 }
