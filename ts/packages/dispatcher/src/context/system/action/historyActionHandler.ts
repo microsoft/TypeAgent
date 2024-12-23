44f291254d63ec8ea944648b38d9376b737d765c
robgruen <robgruen@microsoft.com>
2024-12-23 11:18:24 -0800

    Tightened up help output & added action commands for some system commands (#514)
    - fixed some @ command actions
    - added some more @ command actions
    - help command now outputs things as a table

---
+++ ts/packages/dispatcher/src/context/system/action/historyActionHandler.ts
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ActionContext, AppAction } from "@typeagent/agent-sdk";
+import { CommandHandlerContext } from "../../commandHandlerContext.js";
+import { processCommandNoLock } from "../../../command/command.js";
+import {
+    DeleteHistoryAction,
+    HistoryAction,
+} from "../schema/historyActionSchema.js";
+
+export async function executeHistoryAction(
+    action: AppAction,
+    context: ActionContext<CommandHandlerContext>,
+) {
+    const historyAction = action as HistoryAction;
+    switch (historyAction.actionName) {
+        case "delete":
+            const deleteAction = historyAction as DeleteHistoryAction;
+            await processCommandNoLock(
+                `@history delete ${deleteAction.parameters.messageNumber}`,
+                context.sessionContext.agentContext,
+            );
+            break;
+        case "clear":
+            await processCommandNoLock(
+                `@history clear`,
+                context.sessionContext.agentContext,
+            );
+            break;
+        case "list":
+            await processCommandNoLock(
+                `@history list`,
+                context.sessionContext.agentContext,
+            );
+            break;
+        default:
+            throw new Error(`Invalid action name: ${action.actionName}`);
+    }
+    return undefined;
+}
