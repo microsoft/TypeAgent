f0322e1cd5ff2947fabdae8d2a9b2996d6204a2c
hlucco <henry.lucco@highspot.com>
2024-12-25 00:25:21 -0800

    one more copyright

--- ts/examples/chat/src/memory/chatMemory.ts
+++ ts/examples/chat/src/memory/chatMemory.ts
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation and Henry Lucco.
 // Licensed under the MIT License.
 
 import * as knowLib from "knowledge-processor";
