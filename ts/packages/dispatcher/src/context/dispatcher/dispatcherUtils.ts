6b0d8fca16d492914650f999d4625fc307c9b691
Curtis Man <curtism@microsoft.com>
2025-01-23 17:30:41 -0800

    add file

---
+++ ts/packages/dispatcher/src/context/dispatcher/dispatcherUtils.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { AppAction } from "@typeagent/agent-sdk";
+import { UnknownAction } from "./schema/dispatcherActionSchema.js";
+
+export function isUnknownAction(action: AppAction): action is UnknownAction {
+    return action.actionName === "unknown";
+}
