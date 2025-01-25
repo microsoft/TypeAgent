b6623b14fd6742555bc176340a9253e36cc14501
Robert Gruen <robgruen@microsoft.com>
2025-01-25 10:51:40 -0800

    Added new session test

--- ts/packages/shell/test/sessionCommands.spec.ts
+++ ts/packages/shell/test/sessionCommands.spec.ts
@@ -16,6 +16,7 @@ import {
     testSetup,
     waitForAgentMessage,
 } from "./testHelper";
+import { session } from "electron";
 
 // Annotate entire file as serial.
 test.describe.configure({ mode: 'serial' });
@@ -42,6 +43,54 @@ test.describe("@session Commands", () => {
 
         msg = await sendUserRequestAndWaitForResponse(`@history`, mainWindow);
         expect(msg.length, "History NOT cleared!").toBe(0);
+
+        // close the application
+        await exitApplication(mainWindow);
+    });
+
+    test("@session new/delete/list/info", async () => {
+
+        // launch the app
+        const mainWindow: Page = await testSetup();
+
+        // create a new session so we have at least two
+        let msg = await sendUserRequestAndWaitForResponse(`@session new`, mainWindow);
+        expect(msg.toLowerCase()).toContain("new session created: ");
+
+        // get the session count
+        msg = await sendUserRequestAndWaitForResponse(`@session list`, mainWindow);
+        let sessions = msg.split("\n");
+        const originalSessionCount: number = sessions.length;
+        const sessionName: string = sessions[sessions.length - 1];       
+        
+        // issue delete session command
+        msg = await sendUserRequestAndWaitForResponse(`@session delete ${sessions[0]}`, mainWindow);
+        expect(msg.toLowerCase()).toContain("are you sure");
+
+        // click on cancel button
+        await mainWindow.locator(".choice-button", { hasText: "No" }).click();
+        
+        // verify session not deleted
+        msg = await sendUserRequestAndWaitForResponse(`@session list`, mainWindow);
+        sessions = msg.split("\n");
+        expect(sessions.length, "Session accidentally deleted.").toBe(originalSessionCount);
+
+        // reissue delete session command
+        msg = await sendUserRequestAndWaitForResponse(`@session delete ${sessions[0]}`, mainWindow);
+        expect(msg.toLowerCase()).toContain("are you sure");
+        
+        // click on delete button
+        await mainWindow.locator(".choice-button", { hasText: "Yes" }).click();
+
+        // get new session count
+        msg = await sendUserRequestAndWaitForResponse(`@session list`, mainWindow);
+        sessions = msg.split("\n");
+        expect(sessions.length, "Session accidentally deleted.").toBe(originalSessionCount - 1);
+
+        // get session info
+        msg = await sendUserRequestAndWaitForResponse(`@session info`, mainWindow);
+        sessions = msg.split("\n");
+        expect(sessions[1], "Wrong session selected.").toContain(sessionName);
 
         // close the application
         await exitApplication(mainWindow);
