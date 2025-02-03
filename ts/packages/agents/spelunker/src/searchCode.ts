a1e740673a3c091388614e067ca2c98c1580806b
Guido van Rossum <gvanrossum@microsoft.com>
2025-02-02 20:50:39 -0800

    Nicer way of formatting the summaries

--- ts/packages/agents/spelunker/src/searchCode.ts
+++ ts/packages/agents/spelunker/src/searchCode.ts
@@ -398,10 +398,20 @@ function prepareChunks(chunks: Chunk[]): string {
 }
 
 function prepareSummaries(db: sqlite.Database): string {
+    const languageCommentMap: { [key: string]: string } = {
+        python: "#",
+        typescript: "//",
+    };
     const selectAllSummaries = db.prepare(`SELECT * FROM Summaries`);
     const summaryRows: any[] = selectAllSummaries.all();
-    // TODO: format as code: # <summary> / <signature>
-    return JSON.stringify(summaryRows, undefined, 2);
+    const lines: string[] = [];
+    for (const summaryRow of summaryRows) {
+        const comment = languageCommentMap[summaryRow.language] ?? "#";
+        lines.push("");
+        lines.push(`${comment} ${summaryRow.summary}`);
+        lines.push(summaryRow.signature);
+    }
+    return lines.join("\n");
 }
 
 function createTranslator<T extends object>(
@@ -660,8 +670,8 @@ CREATE TABLE IF NOT EXISTS Blobs (
 );
 CREATE TABLE IF NOT EXISTS Summaries (
     chunkId TEXT PRIMARY KEY REFERENCES chunks(chunkId),
+    language TEXT, -- "python", "typescript", etc.
     summary TEXT,
-    shortName TEXT,
     signature TEXT
 )
 `;
@@ -753,7 +763,7 @@ async function summarizeChunkSlice(
     // Enter them into the database
     const db = context.queryContext!.database!;
     const prepInsertSummary = db.prepare(`
-        INSERT OR REPLACE INTO Summaries (chunkId, summary, signature) VALUES (?, ?, ?)
+        INSERT OR REPLACE INTO Summaries (chunkId, language, summary, signature) VALUES (?, ?, ?, ?)
     `);
     let errors = 0;
     for (const summary of summarizeSpecs.summaries) {
@@ -761,6 +771,7 @@ async function summarizeChunkSlice(
         try {
             prepInsertSummary.run(
                 summary.chunkId,
+                summary.language,
                 summary.summary,
                 summary.signature,
             );
