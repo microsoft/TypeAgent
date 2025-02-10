8c8d26dfaaffa8151b26bd51e4fe937779fd9df4
Guido van Rossum <gvanrossum@microsoft.com>
2025-02-10 13:28:48 -0800

    Improve descriptions in SelectorSchema

--- ts/packages/agents/spelunker/src/selectorSchema.ts
+++ ts/packages/agents/spelunker/src/selectorSchema.ts
@@ -4,15 +4,15 @@
 // Identifier for a chunk of code.
 export type ChunkId = string;
 
-// A chunk is a function/method, class, module/file, interface, type, etc.
-// Nested chunks have been elided from the chunk text (they are their own chunk).
+// Indicates the relevance of a chunk (from the context) to the user question.
+// Only report chunks that have medium-to-high relevance to the question.
 export type ChunkDescription = {
     chunkId: ChunkId;
-    relevance: number; // Float between 0.0 and 1.0
+    relevance: number; // Float between 0.0 and 1.0 giving how relevant this chunk is to the question.
 };
 
-// Only produce at most 30 chunks that are most relevant to the question.
+// Only report chunks that have medium-to-high relevance to the question.
 export type SelectorSpecs = {
-    chunks: ChunkDescription[]; // Empty is fine too, if nothing looks relevant.
+    chunkDescs: ChunkDescription[]; // Empty is fine too, if no chunk looks relevant.
     error?: string; // In case nothing appears relevant.
 };
