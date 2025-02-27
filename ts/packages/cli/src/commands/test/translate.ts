7f6086cfe2bf645f0b5c66b8bb590f9b098c95ae
Curtis Man <curtism@microsoft.com>
2025-02-27 14:03:49 -0800

    More switches

--- ts/packages/cli/src/commands/test/translate.ts
+++ ts/packages/cli/src/commands/test/translate.ts
@@ -126,6 +126,25 @@ export default class TestTranslateCommand extends Command {
             default: true, // follow DispatcherOptions default
             allowNo: true,
         }),
+        schemaOptimization: Flags.boolean({
+            description: "Enable schema optimization",
+        }),
+        switchEmbedding: Flags.boolean({
+            description: "Use embedding to determine the first schema to use",
+            default: true, // follow DispatcherOptions default
+            allowNo: true,
+        }),
+        switchInline: Flags.boolean({
+            description: "Use inline switch schema to select schema group",
+            default: true, // follow DispatcherOptions default
+            allowNo: true,
+        }),
+        switchSearch: Flags.boolean({
+            description:
+                "Enable second chance full switch schema to find schema group",
+            default: true, // follow DispatcherOptions default
+            allowNo: true,
+        }),
         stream: Flags.boolean({
             description: "Enable streaming",
             default: true, // follow DispatcherOptions default
@@ -327,6 +346,14 @@ export default class TestTranslateCommand extends Command {
                             jsonSchemaFunction: flags.jsonSchemaFunction,
                             jsonSchemaValidate: flags.jsonSchemaValidate,
                         },
+                        optimize: {
+                            enabled: flags.schemaOptimization,
+                        },
+                    },
+                    switch: {
+                        embedding: flags.switchEmbedding,
+                        inline: flags.switchInline,
+                        search: flags.switchSearch,
                     },
                 },
                 explainer: { enabled: false },
