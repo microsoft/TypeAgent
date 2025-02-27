6015ce2302cb8d086e5e91c1e47718025eabe919
Robert Gruen <robgruen@microsoft.com>
2025-02-26 23:34:51 -0800

    adding tests

---
+++ ts/packages/defaultAgentProvider/test/translateImageConversation.spec.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { defineTranslateTest } from "./translateTestCommon.js";
+const dataFiles = ["test/data/translate-image-history-e2e.json"];
+
+
+
+describe("RequestAction toString <=> fromString", () => {
+    console.log('test');
+});
+
+describe(`XXX`, async () => {
+    await defineTranslateTest("translate image request (w/history)", dataFiles);
+});
