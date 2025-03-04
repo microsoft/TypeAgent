0c81e51f094c398c61e09b97104b159a69de6aa2
Umesh Madan <umeshma@microsoft.com>
2025-03-04 14:43:10 -0800

    knowpro: natural language querying (phase 1) (#783)
    Part 1:
    - Start by using knowlege-processor to translate natural language
    queries into TermFilters
    - Transforms term filters (terms, date ranges) into know-pro
    **searchConversation** call
    - Execute query
    
    Next check-in will also transform actions.

--- ts/examples/chat/src/memory/knowproCommon.ts
+++ ts/examples/chat/src/memory/knowproCommon.ts
@@ -1,22 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import * as knowLib from "knowledge-processor";
 import * as kp from "knowpro";
-
-export function getTimeRangeForConversation(
-    conversation: kp.IConversation,
-): kp.DateRange | undefined {
-    const messages = conversation.messages;
-    const start = messages[0].timestamp;
-    const end = messages[messages.length - 1].timestamp;
-    if (start !== undefined) {
-        return {
-            start: new Date(start),
-            end: end ? new Date(end) : undefined,
-        };
-    }
-    return undefined;
-}
 
 export function textLocationToString(location: kp.TextLocation): string {
     let text = `MessageIndex: ${location.messageIndex}`;
@@ -27,4 +13,62 @@ export function textLocationToString(location: kp.TextLocation): string {
         text += `\nCharIndex: ${location.charIndex}`;
     }
     return text;
+}
+
+export async function matchFilterToConversation(
+    conversation: kp.IConversation,
+    filter: knowLib.conversation.TermFilterV2,
+    knowledgeType?: kp.KnowledgeType | undefined,
+    useAnd: boolean = false,
+) {
+    let searchTermGroup: kp.SearchTermGroup = termFilterToSearchGroup(
+        filter,
+        useAnd,
+    );
+    let when: kp.WhenFilter = termFilterToWhenFilter(filter);
+    when.knowledgeType = knowledgeType;
+    let searchResults = await kp.searchConversation(
+        conversation,
+        searchTermGroup,
+        when,
+    );
+    if (useAnd && (!searchResults || searchResults.size === 0)) {
+        // Try again with OR
+        searchTermGroup = termFilterToSearchGroup(filter, false);
+        searchResults = await kp.searchConversation(
+            conversation,
+            searchTermGroup,
+            when,
+        );
+    }
+    return searchResults;
+}
+
+export function termFilterToSearchGroup(
+    filter: knowLib.conversation.TermFilterV2,
+    and: boolean,
+): kp.SearchTermGroup {
+    const searchTermGroup: kp.SearchTermGroup = {
+        booleanOp: and ? "and" : "or",
+        terms: [],
+    };
+    if (filter.searchTerms && filter.searchTerms.length > 0) {
+        for (const st of filter.searchTerms) {
+            searchTermGroup.terms.push({ term: { text: st } });
+        }
+    }
+    return searchTermGroup;
+}
+
+export function termFilterToWhenFilter(
+    filter: knowLib.conversation.TermFilterV2,
+) {
+    let when: kp.WhenFilter = {};
+    if (filter.timeRange) {
+        when.dateRange = {
+            start: knowLib.conversation.toStartDate(filter.timeRange.startDate),
+            end: knowLib.conversation.toStopDate(filter.timeRange.stopDate),
+        };
+    }
+    return when;
 }
