2bcfd49fc48612c914e3b2c0180eef34259d1eb3
Robert Gruen <robgruen@microsoft.com>
2025-03-11 15:29:58 -0700

    added copyright

--- ts/packages/dispatcher/src/context/system/handlers/helpCommandHandler.ts
+++ ts/packages/dispatcher/src/context/system/handlers/helpCommandHandler.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { ActionContext, ParsedCommandParams } from "@typeagent/agent-sdk";
 import { CommandHandler } from "@typeagent/agent-sdk/helpers/command";
 import { CommandHandlerContext } from "../../commandHandlerContext.js";
