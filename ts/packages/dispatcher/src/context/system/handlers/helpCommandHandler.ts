34770dbb880fc996037e6f517c09c266375afb3e
Robert Gruen <robgruen@microsoft.com>
2025-03-11 15:24:23 -0700

    ran prettier

--- ts/packages/dispatcher/src/context/system/handlers/helpCommandHandler.ts
+++ ts/packages/dispatcher/src/context/system/handlers/helpCommandHandler.ts
@@ -55,7 +55,11 @@ export class HelpCommandHandler implements CommandHandler {
                         agentNames[i],
                     );
 
-                if (agent !== undefined && agent.getCommands && agentNames[i] == "system") {
+                if (
+                    agent !== undefined &&
+                    agent.getCommands &&
+                    agentNames[i] == "system"
+                ) {
                     printAllCommandsWithUsage(
                         await agent.getCommands!(context.sessionContext),
                         agentNames[i],
