f8bb760fa7ccfd347d30af87bba3a137c75ee74e
gvanrossum-ms <gvanrossum@microsoft.com>
2025-03-14 15:58:22 -0700

    [python/knowpro] Got the first knowledge extraction working (#823)
    - No secondary indexes yet.
    - Auth is a mess (see auth.py).
    - Lots of infrastructure flexibility is missing.
    - No searching.
    
    But I am still excited.

--- python/ta/typeagent/memconv/__main__.py
+++ python/ta/typeagent/memconv/__main__.py
@@ -2,30 +2,34 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import argparse
-from datetime import datetime as Datetime
-import json
 import sys
-from typing import cast
-
-from ..knowpro.convindex import ConversationIndex
 
 minver = (3, 12)
 assert sys.version_info >= minver, f"Needs Python {minver[0]}.{minver[1]}+"
 del minver
 
-# TODO: Get rid of this dummy import.
-import typechat
+import argparse
+from datetime import datetime as Datetime
+import os
+import sys
+from typing import cast
 
-from ..knowpro.interfaces import IndexingEventHandlers, TextLocation
-from ..memconv.import_podcasts import import_podcast
+import dotenv
+
+from typeagent.knowpro.convindex import ConversationIndex
+
+from typeagent.knowpro.interfaces import IndexingEventHandlers, TextLocation
+from typeagent.memconv.import_podcasts import import_podcast
 
 
 async def main():
+    dotenv.load_dotenv(os.path.expanduser("~/TypeAgent/ts/.env"))  # TODO: Only works in dev tree
     parser = argparse.ArgumentParser(description="Import a podcast")
-    parser.add_argument("filename", help="The filename to import")
+    parser.add_argument("filename", nargs="?", help="The filename to import")
     # TODO: Add more arguments for the import_podcast function.
     args = parser.parse_args()
+    if not args.filename:
+        args.filename = os.path.expanduser("~/TypeAgent/python/ta/testdata/npr.txt")
     pod = import_podcast(args.filename, None, Datetime.now(), 3.0)
     print("Name-Tag:", pod.name_tag)
     print("Tags:", ", ".join(pod.tags))
@@ -34,7 +38,7 @@ async def main():
         print(msg)
 
     def on_knowledge_extracted(chunk, knowledge_result) -> bool:
-        print("Knowledge extracted:", chunk, knowledge_result)
+        print("Knowledge extracted:", chunk, "\n    ", knowledge_result)
         return True
 
     def on_embeddings_created(source_texts, batch, batch_start_at) -> bool:
@@ -54,10 +58,9 @@ async def main():
         on_embeddings_created,
         on_text_indexed,
     )
-    print()
     await pod.build_index(handler)
+    print()
     if pod.semantic_ref_index is not None:
-
         data = pod.semantic_ref_index.serialize()
         new = ConversationIndex(data)
         assert new.serialize() == data
