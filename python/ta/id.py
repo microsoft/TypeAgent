ad98b8f8924c14a9c67fa8c6559e2a66491959d6
Guido van Rossum <gvanrossum@microsoft.com>
2025-03-14 13:21:53 -0700

    Improve auth procedure

--- python/ta/id.py
+++
@@ -1,5 +0,0 @@
-from azure.identity import DeviceCodeCredential
-
-credential = DeviceCodeCredential()
-token = credential.get_token("https://cognitiveservices.azure.com/.default")
-print(f"export AZURE_OPENAI_API_KEY={token.token}")
