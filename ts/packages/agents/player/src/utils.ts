3411de6af85315f8d9e35f05829f0053cbbf6731
Curtis Man <curtism@microsoft.com>
2025-04-11 16:48:38 -0700

    Music play use fetch instead of axios (#940)
    Also clean up the endpoint calling code.

--- ts/packages/agents/player/src/utils.ts
+++ ts/packages/agents/player/src/utils.ts
@@ -1,22 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import axios from "axios";
-
-export const translateAxiosError = (e: any, url?: string) => {
-    throw new Error(translateAxiosErrorNoThrow(e, url));
-};
-
-export const translateAxiosErrorNoThrow = (e: any, url?: string) => {
-    if (e instanceof axios.AxiosError) {
-        const responseData = e.response?.data;
-        const dataString: string =
-            typeof responseData === "object"
-                ? JSON.stringify(responseData)
-                : responseData;
-
-        return `${e.message}: ${url ? `${url} ` : ""}${dataString}`;
-    } else {
-        return e;
+export async function createFetchError(message: string, response: Response) {
+    let responseData = "";
+    try {
+        responseData = `\n${await response.text()}`;
+    } catch {
+        // Ignore error
     }
-};
+    return new Error(
+        `${message}: ${response.status} ${response.statusText}${responseData}`,
+    );
+}
