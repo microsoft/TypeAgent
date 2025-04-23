5345c1acf487154e7e8f2c4ba93b6d4302e62e80
Umesh Madan <umeshma@microsoft.com>
2025-04-22 17:38:18 -0700

    Remove hardcoded schema

--- ts/packages/knowPro/src/hybrid/knownFacetsSchema.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export type KnownFacet =
-    | "restaurant.rating"
-    | "restaurant.city"
-    | "restaurant.country";
