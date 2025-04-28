c9591c1473abbc68a31ef8ea46710e40511704d3
Guido van Rossum <gvanrossum@microsoft.com>
2025-04-28 15:07:56 -0700

    Add a fixture that provides a temp directory that's cleaned up afterwards

--- python/ta/test/fixtures.py
+++ python/ta/test/fixtures.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+import tempfile
+
 import pytest
 
 from typeagent.aitools import auth
@@ -9,3 +11,8 @@ from typeagent.aitools import auth
 @pytest.fixture(scope="session")
 def needs_auth():
     auth.load_dotenv()
+
+@pytest.fixture
+def temp_dir():
+    with tempfile.TemporaryDirectory() as dir:
+        yield dir
