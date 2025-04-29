6c9924fe06dadc7bee2c0c4f2a9a429568dc353f
Robert Gruen <robgruen@microsoft.com>
2025-04-29 09:07:00 -0700

    WIP dataframe integratinon

---
+++ ts/packages/memory/image/src/imageCollection.ts
@@ -0,0 +1,239 @@
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+    IConversation,
+    IMessage,
+    SemanticRef,
+    ConversationIndex,
+    IndexingResults,
+    ConversationSettings,
+    createConversationSettings,
+    addMessageKnowledgeToSemanticRefIndex,
+    buildSecondaryIndexes,
+    ConversationSecondaryIndexes,
+    IndexingEventHandlers,
+    IConversationDataWithIndexes,
+    writeConversationDataToFile,
+    readConversationDataFromFile,
+    buildTransientSecondaryIndexes,
+    readConversationDataFromBuffer,
+    hybrid
+} from "knowpro";
+import {
+    createEmbeddingCache,
+} from "knowledge-processor";
+import { openai, TextEmbeddingModel } from "aiclient";
+//import registerDebug from "debug";
+import sqlite from "better-sqlite3";
+import * as ms from "memory-storage";
+import { ExposureTable, GeoTable } from "./tables.js";
+import { Image, ImageMeta } from "./imageMeta.js";
+
+//const debug = registerDebug("typeagent:image-memory");
+
+export interface ImageCollectionData
+    extends IConversationDataWithIndexes<Image> {}
+
+export class ImageCollection implements IConversation, hybrid.IConversationHybrid {
+    public settings: ConversationSettings;
+    public semanticRefIndex: ConversationIndex;
+    public secondaryIndexes: ConversationSecondaryIndexes;
+
+    // Data frames for typed image meta data
+    public dataFrames: hybrid.DataFrameCollection;
+    public locations: hybrid.IDataFrame;
+    public exposure: hybrid.IDataFrame;
+
+    constructor(
+        public nameTag: string = "",
+        public messages: Image[] = [],
+        public tags: string[] = [],
+        public semanticRefs: SemanticRef[] = [],
+        public dbPath: string = "",
+        private db: sqlite.Database | undefined = undefined,
+    ) {
+        const [model, embeddingSize] = this.createEmbeddingModel();
+        this.settings = createConversationSettings(model, embeddingSize);
+        this.semanticRefIndex = new ConversationIndex();
+        this.secondaryIndexes = new ConversationSecondaryIndexes(this.settings);
+
+        // create dataFrames (tables)
+        this.db = ms.sqlite.createDatabase(dbPath, true);
+        this.locations = new GeoTable(this.db);
+        this.exposure = new ExposureTable(this.db);
+
+        // create dataFrames collection
+        // TODO: select other Facets/meta data fields
+        this.dataFrames = new Map<string, hybrid.IDataFrame>([
+            [this.locations.name, this.locations],
+            [this.exposure.name, this.exposure]
+        ]);
+    }
+
+    get conversation(): IConversation<IMessage> {
+        return this;
+    }
+
+    public addMetadataToIndex() {
+        if (this.semanticRefIndex) {
+            addMessageKnowledgeToSemanticRefIndex(
+                this,
+                0,
+            );
+        }
+    }
+
+    /*
+    * Enumerates the messages and adds them to the data frame.
+    */
+    public addMetadataToDataFrames() {
+        if (this.semanticRefIndex) {
+            this.messages.forEach(
+                (img: Image, index: number) => {
+
+                    // add image location to dataframe
+                    const latlong = img.metadata.dataFrames[this.locations.name];
+                    if (this.locations && latlong) {
+                        this.locations.addRows({ sourceRef: img, record: latlong});
+                    } 
+
+                    // add exposure to dataframe
+                    const exposure = img.metadata.dataFrames[this.exposure.name];
+                    if (this.exposure && exposure) {
+                        this.exposure.addRows(exposure);
+                    } 
+
+                    // TODO: add additional meta data tables
+                }
+            )
+        }
+    }
+
+    public async buildIndex(
+        eventHandler?: IndexingEventHandlers,
+    ): Promise<IndexingResults> {
+        //const result = await buildConversationIndex(this, eventHandler);
+        this.semanticRefIndex = new ConversationIndex();
+        if (this.semanticRefs === undefined) {
+            this.semanticRefs = [];
+        }
+
+        this.addMetadataToIndex();
+        this.addMetadataToDataFrames();
+        const indexingResult: IndexingResults = {
+            semanticRefs: {
+                completedUpto: { messageOrdinal: this.messages.length - 1 },
+            },
+        };
+        indexingResult.secondaryIndexResults = await buildSecondaryIndexes(
+            this,
+            this.settings,
+            eventHandler,
+        );
+
+        return indexingResult;
+    }
+
+    public async serialize(): Promise<ImageCollectionData> {
+        const conversationData: ImageCollectionData = {
+            nameTag: this.nameTag,
+            messages: this.messages,
+            tags: this.tags,
+            semanticRefs: this.semanticRefs,
+            semanticIndexData: this.semanticRefIndex?.serialize(),
+            relatedTermsIndexData:
+                this.secondaryIndexes.termToRelatedTermsIndex.serialize(),
+        };
+        return conversationData;
+    }
+
+    public async deserialize(data: ImageCollectionData): Promise<void> {
+        this.nameTag = data.nameTag;
+        const messages = data.messages.map((m) => {
+            const image = new Image(
+                m.textChunks,
+                new ImageMeta(m.metadata.fileName, m.metadata.img),
+                m.tags,
+            );
+            image.timestamp = m.timestamp;
+            return image;
+        });
+        this.messages = messages;
+        this.semanticRefs = data.semanticRefs;
+        this.tags = data.tags;
+        if (data.semanticIndexData) {
+            this.semanticRefIndex = new ConversationIndex(
+                data.semanticIndexData,
+            );
+        }
+        if (data.relatedTermsIndexData) {
+            this.secondaryIndexes.termToRelatedTermsIndex.deserialize(
+                data.relatedTermsIndexData,
+            );
+        }
+        await buildTransientSecondaryIndexes(this, this.settings);
+    }
+
+    public async writeToFile(
+        dirPath: string,
+        baseFileName: string,
+    ): Promise<void> {
+        const data = await this.serialize();
+        await writeConversationDataToFile(data, dirPath, baseFileName);
+    }
+
+    public static async readFromFile(
+        dirPath: string,
+        baseFileName: string,
+    ): Promise<ImageCollection | undefined> {
+        const imageCollection = new ImageCollection();
+        const data = await readConversationDataFromFile(
+            dirPath,
+            baseFileName,
+            imageCollection.settings.relatedTermIndexSettings
+                .embeddingIndexSettings?.embeddingSize,
+        );
+        if (data) {
+            imageCollection.deserialize(data);
+        }
+        return imageCollection;
+    }
+
+    public static async fromBuffer(
+        jsonData: string,
+        embeddingsBuffer: Buffer,
+    ): Promise<ImageCollection> {
+        const imageCollection = new ImageCollection();
+
+        const data = await readConversationDataFromBuffer(
+            jsonData,
+            embeddingsBuffer,
+            imageCollection.settings.relatedTermIndexSettings
+                .embeddingIndexSettings?.embeddingSize,
+        );
+
+        if (data) {
+            imageCollection.deserialize(data);
+        }
+
+        return imageCollection;
+    }
+
+    /**
+     * Our index already has embeddings for every term in the podcast
+     * Create an embedding model that can just leverage those embeddings
+     * @returns embedding model, size of embedding
+     */
+    private createEmbeddingModel(): [TextEmbeddingModel, number] {
+        return [
+            createEmbeddingCache(
+                openai.createEmbeddingModel(),
+                64,
+                () => this.secondaryIndexes.termToRelatedTermsIndex.fuzzyIndex,
+            ),
+            1536,
+        ];
+    }
+}
---
+++ ts/packages/memory/image/src/imageMeta.ts
@@ -0,0 +1,399 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+    hybrid,
+    IKnowledgeSource,
+    IMessage,
+    IMessageMetadata,
+} from "knowpro";
+import {
+    conversation as kpLib,
+    image,
+} from "knowledge-processor";
+import path from "node:path";
+import { AddressOutput } from "@azure-rest/maps-search";
+import { DataFrameValue } from "../../../knowPro/dist/hybrid/dataFrame.js";
+//import registerDebug from "debug";
+
+//const debug = registerDebug("typeagent:image-memory");
+
+export class Image implements IMessage {
+    public timestamp: string | undefined;
+    constructor(
+        public textChunks: string[],
+        public metadata: ImageMeta,
+        public tags: string[] = [],
+    ) {
+        this.timestamp = metadata.img.dateTaken;
+    }
+    getKnowledge(): kpLib.KnowledgeResponse {
+        return this.metadata.getKnowledge();
+    }
+}
+
+// metadata for images
+export class ImageMeta implements IKnowledgeSource, IMessageMetadata {
+
+    public imageEntity: kpLib.ConcreteEntity | undefined = undefined;
+    public dataFrameRecords: Record<string, hybrid.DataFrameRecord> = {};
+
+    constructor(
+        public fileName: string,
+        public img: image.Image,
+    ) {}
+
+    public get source() {
+        return undefined;
+    }
+    public get dest() {
+        return undefined;
+    }
+
+    getKnowledge() {
+        this.imageEntity = {
+            name: `${path.basename(this.img.fileName)} - ${this.img.title}`,
+            type: ["file", "image"],
+            facets: [
+                { name: "File Name", value: this.img.fileName },
+                //{ name: "Title", value: this.image.title },
+                //{ name: "Caption", value: this.image.title },
+            ],
+        };
+
+        // EXIF data are facets of this image
+        const latLongDataFrame: hybrid.DataFrameRecord = {}
+        for (let i = 0; i < this.img?.exifData.length; i++) {
+            
+            const exifPropertyName = this.img?.exifData[i];
+            const exifPropertyValue = this.img?.exifData[1];
+
+            if (exifPropertyName && exifPropertyValue) {
+                this.imageEntity.facets!.push({
+                    name: this.img?.exifData[i][0],
+                    value: this.img?.exifData[i][1],
+                });
+
+                // save the dataFrame record
+                this.dataFrameRecords[exifPropertyName] = exifPropertyValue;
+
+                // TODO: add some of these to the dataFrames
+                // switch (exifPropertyName) {
+                //     case "GPSLatitude": {
+                //         latLongDataFrame[exifPropertyName] = exifPropertyValue as number;
+                //         break;
+                //     }
+                //     case "GPSLatitudeRef": {
+                //         break;
+                //     }
+                //     case "GPSLongitude": {
+                //         break;
+                //     }
+                //     case "GPSLongitudeRef": {
+                //         break;
+                //     }
+                //     default: {
+                //         break;
+                //     }
+                //}
+            }
+        }
+
+        // construct the necessary data frames that the index is expecting
+        if (this.hasGeo()) {
+            this.dataFrameRecords["geo"] = this.getGeo();
+        }
+
+
+        // create the return values
+        let entities: kpLib.ConcreteEntity[] = [];
+        let actions: kpLib.Action[] = [];
+        let inverseActions: kpLib.Action[] = [];
+        let topics: kpLib.Topic[] = [];
+        const timestampParam = [];
+
+        if (this.img.dateTaken) {
+            timestampParam.push({
+                name: "Timestamp",
+                value: this.img.dateTaken,
+            });
+        }
+
+        // add the image entity
+        entities.push(this.imageEntity);
+
+        // if we have POI those are also entities
+        if (this.img.nearbyPOI) {
+            for (let i = 0; i < this.img.nearbyPOI.length; i++) {
+                const poiEntity: kpLib.ConcreteEntity = {
+                    name: this.img.nearbyPOI[i].name!,
+                    type: [
+                        ...this.img.nearbyPOI[i].categories!,
+                        "PointOfInterest",
+                    ],
+                    facets: [],
+                };
+
+                if (this.img.nearbyPOI[i].freeFormAddress) {
+                    poiEntity.facets?.push({
+                        name: "address",
+                        value: this.img.nearbyPOI[i].freeFormAddress!,
+                    });
+                }
+
+                if (
+                    this.img.nearbyPOI[i].position != undefined &&
+                    this.img.nearbyPOI[i].position?.latitude != undefined &&
+                    this.img.nearbyPOI[i].position?.longitude != undefined
+                ) {
+                    poiEntity.facets?.push({
+                        name: "position",
+                        value: JSON.stringify(this.img.nearbyPOI[i].position),
+                    });
+                    poiEntity.facets?.push({
+                        name: "longitude",
+                        value: this.img.nearbyPOI[
+                            i
+                        ].position!.longitude!.toString(),
+                    });
+                    poiEntity.facets?.push({
+                        name: "latitude",
+                        value: this.img.nearbyPOI[
+                            i
+                        ].position!.latitude!.toString(),
+                    });
+                }
+
+                if (
+                    this.img.nearbyPOI[i].categories !== undefined &&
+                    this.img.nearbyPOI[i].categories!.length > 0
+                ) {
+                    poiEntity.facets?.push({
+                        name: "category",
+                        value: this.img.nearbyPOI[i].categories!.join(","),
+                    });
+                }
+
+                entities.push(poiEntity);
+
+                actions.push({
+                    verbs: ["near"],
+                    verbTense: "present",
+                    subjectEntityName: this.img.fileName,
+                    objectEntityName: this.img.nearbyPOI[i].name!,
+                    indirectObjectEntityName: "ME", // TODO: image taker name
+                });
+            }
+        }
+
+        // reverse lookup addresses are also entities
+        if (this.img.reverseGeocode) {
+            for (let i = 0; i < this.img.reverseGeocode.length; i++) {
+                // only put in high confidence items or the first one
+                if (
+                    (i == 0 ||
+                        this.img.reverseGeocode[i].confidence == "High") &&
+                    this.img.reverseGeocode[i].address !== undefined
+                ) {
+                    const addrOutput: AddressOutput =
+                        this.img.reverseGeocode[i].address!;
+                    const addrEntity: kpLib.ConcreteEntity = {
+                        name:
+                            this.img.reverseGeocode[i].address!
+                                .formattedAddress ?? "",
+                        type: ["address"],
+                        facets: [],
+                    };
+
+                    // Add the address of the image as a facet to it's entity
+                    if (i == 0 && addrOutput.formattedAddress) {
+                        this.imageEntity.facets?.push({
+                            name: "address",
+                            value: addrOutput.formattedAddress,
+                        });
+                    }
+
+                    // make the address an entity
+                    entities.push(addrEntity);
+
+                    // now make an entity for all of the different parts of the address
+                    // and add them as facets to the address
+                    if (addrOutput.addressLine) {
+                        addrEntity.facets?.push({
+                            name: "addressLine",
+                            value: addrOutput.addressLine,
+                        });
+                        entities.push({
+                            name: addrOutput.locality ?? "",
+                            type: ["locality", "place"],
+                        });
+                    }
+                    if (addrOutput.locality) {
+                        addrEntity.facets?.push({
+                            name: "locality",
+                            value: addrOutput.locality,
+                        });
+                    }
+                    if (addrOutput.neighborhood) {
+                        addrEntity.facets?.push({
+                            name: "neighborhood",
+                            value: addrOutput.neighborhood,
+                        });
+                        entities.push({
+                            name: addrOutput.neighborhood ?? "",
+                            type: ["neighborhood", "place"],
+                        });
+                    }
+                    if (addrOutput.adminDistricts) {
+                        addrEntity.facets?.push({
+                            name: "district",
+                            value: JSON.stringify(addrOutput.adminDistricts),
+                        });
+                        for (
+                            let i = 0;
+                            i < addrOutput.adminDistricts.length;
+                            i++
+                        ) {
+                            const e: kpLib.ConcreteEntity = {
+                                name: addrOutput.adminDistricts[i].name ?? "",
+                                type: ["district", "place"],
+                                facets: [],
+                            };
+
+                            if (addrOutput.adminDistricts[i].shortName) {
+                                e.facets?.push({
+                                    name: "shortName",
+                                    value: addrOutput.adminDistricts[i]
+                                        .shortName!,
+                                });
+                            }
+
+                            entities.push(e);
+                        }
+                    }
+                    if (addrOutput.postalCode) {
+                        addrEntity.facets?.push({
+                            name: "postalCode",
+                            value: addrOutput.postalCode,
+                        });
+                        entities.push({
+                            name: addrOutput.postalCode ?? "",
+                            type: ["postalCode", "place"],
+                        });
+                    }
+                    if (addrOutput.countryRegion) {
+                        if (
+                            addrOutput.countryRegion.name !== undefined &&
+                            addrOutput.countryRegion.name.length > 0
+                        ) {
+                            addrEntity.facets?.push({
+                                name: "countryName",
+                                value: addrOutput.countryRegion?.name!,
+                            });
+                        }
+                        if (
+                            addrOutput.countryRegion.ISO !== undefined &&
+                            addrOutput.countryRegion.ISO.length > 0
+                        ) {
+                            addrEntity.facets?.push({
+                                name: "countryISO",
+                                value: addrOutput.countryRegion?.ISO!,
+                            });
+                        }
+                        entities.push({
+                            name: addrOutput.countryRegion.name ?? "",
+                            type: ["country", "place"],
+                            //facets: [ { name: "ISO", value: addrOutput.countryRegion.ISO ?? "" } ]
+                        });
+                    }
+                    if (addrOutput.intersection) {
+                        addrEntity.facets?.push({
+                            name: "intersection",
+                            value: JSON.stringify(addrOutput.intersection),
+                        });
+                        entities.push({
+                            name: addrOutput.intersection.displayName ?? "",
+                            type: ["intersection", "place"],
+                        });
+                    }
+
+                    actions.push({
+                        verbs: ["captured at"],
+                        verbTense: "present",
+                        subjectEntityName: this.imageEntity.name,
+                        objectEntityName: addrEntity.name,
+                        params: timestampParam,
+                        indirectObjectEntityName: "none",
+                    });
+                }
+            }
+        }
+
+        // add knowledge response items from ImageMeta to knowledge
+        if (this.img.knowledge?.entities) {
+            entities = entities.concat(this.img.knowledge?.entities);
+
+            // each extracted entity "is in" the image
+            // and all such entities are "contained by" the image
+            for (let i = 0; i < this.img.knowledge?.entities.length; i++) {
+                actions.push({
+                    verbs: ["within"],
+                    verbTense: "present",
+                    subjectEntityName: this.img.knowledge?.entities[i].name,
+                    objectEntityName: this.imageEntity.name,
+                    indirectObjectEntityName: "none",
+                    params: timestampParam,
+                    subjectEntityFacet: undefined,
+                });
+
+                actions.push({
+                    verbs: ["contains"],
+                    verbTense: "present",
+                    subjectEntityName: this.imageEntity.name,
+                    objectEntityName: this.img.knowledge.entities[i].name,
+                    indirectObjectEntityName: "none",
+                    params: timestampParam,
+                    subjectEntityFacet: undefined,
+                });
+            }
+        }
+        if (this.img.knowledge?.actions) {
+            actions = actions.concat(this.img.knowledge?.actions);
+        }
+        if (this.img.knowledge?.inverseActions) {
+            inverseActions = actions.concat(this.img.knowledge?.inverseActions);
+        }
+        if (this.img.knowledge?.topics) {
+            topics = topics.concat(this.img.knowledge.topics);
+        }
+
+        return {
+            entities,
+            actions,
+            inverseActions: inverseActions,
+            topics: topics,
+        };
+    }
+
+    hasGeo(): boolean {
+
+        if (this.dataFrameRecords["GPSLatitude"]
+            && this.dataFrameRecords["GPSLatitudeRef"]
+            && this.dataFrameRecords["GPSLongitude"]
+            && this.dataFrameRecords["GPSLongitudeRef"]) {
+                return true;
+        }
+
+        return false;
+    }
+
+    getGeo(): hybrid.DataFrameRecord {
+        const latRef: hybrid.DataFrameRecord = this.dataFrameRecords["GPSLatitudeRef"];
+        const longRef: hybrid.DataFrameRecord  = this.dataFrameRecords["GPSLongitudeRef"];
+
+        // TODO: implement
+        if (latRef)
+
+        return {"dafd": 'sdfsd'};
+    }
+}
