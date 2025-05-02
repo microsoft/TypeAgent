52df6e3c1daabd14063a8bb1c3b8564bdc5b7c1c
Curtis Man <curtism@microsoft.com>
2025-05-01 22:47:25 -0700

    status

--- ts/packages/shell/src/main/commands/update.ts
+++ ts/packages/shell/src/main/commands/update.ts
@@ -220,10 +220,10 @@ async function checkUpdate(
     const result = await autoUpdater.checkForUpdates();
 
     if (install) {
-        if (url !== undefined || channel !== undefined) {
-            state.lastChecked = new Date();
-            if (result !== null && result.isUpdateAvailable) {
-                state.updateInfo = result.updateInfo;
+        state.lastChecked = new Date();
+        if (result !== null && result.isUpdateAvailable) {
+            state.updateInfo = result.updateInfo;
+            if (url !== undefined || channel !== undefined) {
                 stopBackgroundUpdateCheck();
                 // If we have a custom update queued, don't check again.
                 state.custom = true;
