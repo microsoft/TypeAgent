microsoft/TypeAgent

commit d703008a52685c78532ad6bb0a96b781c535e428

Remove Action and Actions class and use FullAction and the new ExecutableActions. (#671)
- Reduce the number of types we need to deal with related to actions.
- Remove the use of implicit toJson() and toString() from the Action and
Actions class.
- Remove hack in RPC to naturally marshal the actions as `FullAction`
instead of `JSONAction`
- `JSONAction` preserve `resultEntityId` as well

ts/packages/dispatcher/src/translation/actionTemplate.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Action, Actions } from "agent-cache";
+import { ExecutableAction } from "agent-cache";
 import { getActionSchema } from "./actionSchemaFileCache.js";
 import { CommandHandlerContext } from "../context/commandHandlerContext.js";
 import {
@@ -114,17 +114,17 @@ function toTemplateType(type: ActionParamType): TemplateType | undefined {
 function toTemplate(
     context: CommandHandlerContext,
     translators: string[],
-    action: Action,
+    action: ExecutableAction,
 ) {
     const actionSchemaFile = context.agents.tryGetActionSchemaFile(
-        action.translatorName,
+        action.action.translatorName,
     );
     if (actionSchemaFile === undefined) {
         return getDefaultActionTemplate(translators);
     }
     const template = getDefaultActionTemplate(
         translators,
-        action.translatorName,
+        action.action.translatorName,
     );
     const actionSchemas = actionSchemaFile.actionSchemas;
     const actionName: TemplateFieldStringUnion = {
@@ -136,11 +136,11 @@ function toTemplate(
         type: actionName,
     };
 
-    const actionSchema = actionSchemas.get(action.actionName);
+    const actionSchema = actionSchemas.get(action.action.actionName);
     if (actionSchema === undefined) {
         return template;
     }
-    actionName.discriminator = action.actionName;
+    actionName.discriminator = action.action.actionName;
 
     const parameterType = actionSchema.type.fields.parameters?.type;
     if (parameterType) {
@@ -157,7 +157,7 @@ function toTemplate(
 
 export function getActionTemplateEditConfig(
     context: CommandHandlerContext,
-    actions: Actions,
+    actions: ExecutableAction[],
     preface: string,
     editPreface: string,
 ): TemplateEditConfig {
@@ -167,7 +167,7 @@ export function getActionTemplateEditConfig(
     for (const action of actions) {
         templateData.push({
             schema: toTemplate(context, translators, action),
-            data: action.toFullAction(),
+            data: action.action,
         });
     }
 

ts/packages/agentRpc/src/client.ts
@@ -15,6 +15,7 @@ import {
     ClientAction,
     AppAgentManifest,
     Entity,
+    AppAction,
 } from "@typeagent/agent-sdk";
 import {
     AgentCallFunctions,
@@ -311,7 +312,7 @@ export async function createAgentRpcClient(
             });
         },
         executeAction(
-            action: any,
+            action: AppAction,
             context: ActionContext<ShimContext>,
             entityMap: Map<string, Entity>,
         ) {
@@ -324,7 +325,7 @@ export async function createAgentRpcClient(
             );
         },
         validateWildcardMatch(
-            action: any,
+            action: AppAction,
             context: SessionContext<ShimContext>,
         ) {
             return rpc.invoke("validateWildcardMatch", {

ts/packages/agentRpc/src/server.ts
@@ -26,26 +26,10 @@ import {
     AgentContextInvokeFunctions,
     AgentInvokeFunctions,
     ContextParams,
-    JSONAction,
 } from "./types.js";
 import { createRpc } from "./rpc.js";
 import { ChannelProvider } from "./common.js";
 import { createLimiter } from "common-utils";
-
-// TODO: Duplicate code from agent-cache
-function parseActionNameParts(fullActionName: string) {
-    const parts = fullActionName.split(".");
-    const translatorName = parts.slice(0, -1).join(".");
-    const actionName = parts.at(-1)!;
-    return { translatorName, actionName };
-}
-
-function fromJSONAction(action: JSONAction): AppAction {
-    return {
-        ...parseActionNameParts(action.fullActionName),
-        parameters: action.parameters,
-    };
-}
 
 export function createAgentRpcServer(
     name: string,
@@ -80,7 +64,7 @@ export function createAgentRpcServer(
         },
         async executeAction(
             param: Partial<ActionContextParams> & {
-                action: JSONAction;
+                action: AppAction;
                 entityMap: [string, Entity][];
             },
         ): Promise<any> {
@@ -88,10 +72,7 @@ export function createAgentRpcServer(
                 throw new Error("Invalid invocation of executeAction");
             }
             return agent.executeAction(
-                // REVIEW: blah!
-                param.action.fullActionName !== undefined
-                    ? fromJSONAction(param.action)
-                    : (param.action as unknown as AppAction),
+                param.action,
                 getActionContextShim(param),
                 new Map(param.entityMap),
             );
@@ -101,7 +82,7 @@ export function createAgentRpcServer(
                 throw new Error("Invalid invocation of validateWildcardMatch");
             }
             return agent.validateWildcardMatch(
-                fromJSONAction(param.action),
+                param.action,
                 getSessionContextShim(param),
             );
         },

ts/packages/agentRpc/src/types.ts
@@ -19,12 +19,6 @@ import {
     TemplateSchema,
 } from "@typeagent/agent-sdk";
 
-// TODO: Duplicate code from agent-cache
-export interface JSONAction {
-    fullActionName: string;
-    parameters?: Record<string, unknown> | undefined;
-}
-
 export type AgentContextCallFunctions = {
     notify(param: {
         contextId: number;
@@ -122,12 +116,12 @@ export type AgentInvokeFunctions = {
     ) => Promise<void>;
     executeAction: (
         param: Partial<ActionContextParams> & {
-            action: JSONAction;
+            action: AppAction;
             entityMap: [string, Entity][];
         },
     ) => Promise<ActionResult | undefined>;
     validateWildcardMatch: (
-        param: Partial<ContextParams> & { action: JSONAction },
+        param: Partial<ContextParams> & { action: AppAction },
     ) => Promise<boolean>;
     getDynamicDisplay: (
         param: Partial<ContextParams> & {

ts/packages/cache/src/cache/cache.ts
@@ -4,7 +4,11 @@
 import { DeepPartialUndefined } from "common-utils";
 import * as Telemetry from "telemetry";
 import { ExplanationData } from "../explanation/explanationData.js";
-import { RequestAction } from "../explanation/requestAction.js";
+import {
+    getFullActionName,
+    getTranslationNamesForActions,
+    RequestAction,
+} from "../explanation/requestAction.js";
 import {
     SchemaInfoProvider,
     doCacheAction,
@@ -131,7 +135,7 @@ export class AgentCache {
 
                     if (!cacheAction) {
                         return getFailedResult(
-                            `Caching disabled in schema config for action '${action.fullActionName}'`,
+                            `Caching disabled in schema config for action '${getFullActionName(action)}'`,
                         );
                     }
                 }
@@ -169,7 +173,7 @@ export class AgentCache {
                     message = `Explainer '${this.explainerName}' doesn't support constructions.`;
                 } else {
                     const namespaceKeys = this.getNamespaceKeys(
-                        actions.translatorNames,
+                        getTranslationNamesForActions(actions),
                     );
                     const result = await store.addConstruction(
                         namespaceKeys,

ts/packages/cache/src/cache/explainWorkQueue.ts
@@ -3,6 +3,7 @@
 
 import { QueueObject, queue } from "async";
 import {
+    getTranslationNamesForActions,
     normalizeParamString,
     RequestAction,
 } from "../explanation/requestAction.js";
@@ -24,7 +25,7 @@ function checkExplainableValues(
     const normalizedRequest = normalizeParamString(requestAction.request);
     const pending: unknown[] = [];
 
-    for (const action of requestAction.actions) {
+    for (const { action } of requestAction.actions) {
         pending.push(action.parameters);
     }
 
@@ -111,7 +112,7 @@ export class ExplainWorkQueue {
             const startTime = performance.now();
             const actions = requestAction.actions;
             const explainer = this.getExplainerForTranslator(
-                actions.translatorNames,
+                getTranslationNamesForActions(actions),
                 model,
             );
             const explainerConfig = {

ts/packages/cache/src/constructions/constructions.ts
@@ -4,8 +4,8 @@
 import {
     RequestAction,
     ParamValueType,
-    Actions,
     HistoryContext,
+    fromJsonActions,
 } from "../explanation/requestAction.js";
 import { MatchConfig, matchParts } from "./constructionMatch.js";
 import {
@@ -126,7 +126,7 @@ export class Construction {
                 construction: this,
                 match: new RequestAction(
                     request,
-                    Actions.fromJSON(actionProps),
+                    fromJsonActions(actionProps),
                     config.history,
                 ),
                 conflictValues: matchedValues.conflictValues,

ts/packages/cache/src/constructions/importConstructions.ts
@@ -3,7 +3,11 @@
 
 import { SchemaInfoProvider } from "../explanation/schemaInfoProvider.js";
 import { Construction } from "./constructions.js";
-import { Actions, RequestAction } from "../explanation/requestAction.js";
+import {
+    fromJsonActions,
+    getTranslationNamesForActions,
+    RequestAction,
+} from "../explanation/requestAction.js";
 import {
     ConstructionFactory,
     GenericExplainer,
@@ -62,12 +66,12 @@ function createConstructions(
     for (const entry of data.entries) {
         const requestAction = new RequestAction(
             entry.request,
-            Actions.fromJSON(entry.action),
+            fromJsonActions(entry.action),
         );
 
         try {
             const actions = requestAction.actions;
-            for (const action of actions) {
+            for (const { action } of actions) {
                 if (!dataSchemaNames.has(action.translatorName)) {
                     throw new Error(
                         `Schema name '${action.translatorName}' not found in data header`,
@@ -76,7 +80,7 @@ function createConstructions(
             }
             const explanation = entry.explanation;
             const namespaceKeys = getSchemaNamespaceKeys(
-                actions.translatorNames,
+                getTranslationNamesForActions(actions),
                 schemaInfoProvider,
             );
             const construction = createConstruction(

commit 754f5c62b4ba22739963be18e7971cff4ed04cee

Fix confirm flow (#1082)
- The typing for call to `toTemplate` is incorrect, as it expects an
`ExecutableAction`, but got passed in a `FullAction`.
Regression from #671 
- Change it to accept `FullAction`
- Use `unknown` instead of `any` for safety in `getSystemTemplateSchema`

ts/packages/dispatcher/src/translation/actionTemplate.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { ExecutableAction } from "agent-cache";
+import { ExecutableAction, FullAction } from "agent-cache";
 import { getActionSchema } from "./actionSchemaFileCache.js";
 import { CommandHandlerContext } from "../context/commandHandlerContext.js";
 import {
@@ -114,18 +114,15 @@ function toTemplateType(type: ActionParamType): TemplateType | undefined {
 function toTemplate(
     context: CommandHandlerContext,
     schemas: string[],
-    action: ExecutableAction,
+    action: FullAction,
 ) {
     const actionSchemaFile = context.agents.tryGetActionSchemaFile(
-        action.action.schemaName,
+        action.schemaName,
     );
     if (actionSchemaFile === undefined) {
         return getDefaultActionTemplate(schemas);
     }
-    const template = getDefaultActionTemplate(
-        schemas,
-        action.action.schemaName,
-    );
+    const template = getDefaultActionTemplate(schemas, action.schemaName);
     const actionSchemas = actionSchemaFile.parsedActionSchema.actionSchemas;
     const actionName: TemplateFieldStringUnion = {
         type: "string-union",
@@ -136,11 +133,11 @@ function toTemplate(
         type: actionName,
     };
 
-    const actionSchema = actionSchemas.get(action.action.actionName);
+    const actionSchema = actionSchemas.get(action.actionName);
     if (actionSchema === undefined) {
         return template;
     }
-    actionName.discriminator = action.action.actionName;
+    actionName.discriminator = action.actionName;
 
     const parameterType = actionSchema.type.fields.parameters?.type;
     if (parameterType) {
@@ -166,7 +163,7 @@ export function getActionTemplateEditConfig(
     const schemas = context.agents.getActiveSchemas();
     for (const action of actions) {
         templateData.push({
-            schema: toTemplate(context, schemas, action),
+            schema: toTemplate(context, schemas, action.action),
             data: action.action,
         });
     }
@@ -182,9 +179,23 @@ export function getActionTemplateEditConfig(
     };
 }
 
+function coerceToFullAction(data: unknown): FullAction {
+    if (typeof data !== "object" || data === null) {
+        return { schemaName: "", actionName: "" };
+    }
+    const result = data as FullAction;
+    if (typeof result.schemaName !== "string") {
+        result.schemaName = "";
+    }
+    if (typeof result.actionName !== "string") {
+        result.actionName = "";
+    }
+    return result;
+}
+
 export async function getSystemTemplateSchema(
     templateName: string,
-    data: any,
+    data: unknown,
     context: SessionContext<CommandHandlerContext>,
 ): Promise<TemplateSchema> {
     if (templateName !== "action") {
@@ -192,19 +203,8 @@ export async function getSystemTemplateSchema(
     }
 
     const systemContext = context.agentContext;
-
-    // check user input to make sure it is an action
-
-    if (typeof data.schemaName !== "string") {
-        data.schemaName = "";
-    }
-
-    if (typeof data.actionName !== "string") {
-        data.actionName = "";
-    }
-
     const schemas = systemContext.agents.getActiveSchemas();
-    return toTemplate(systemContext, schemas, data);
+    return toTemplate(systemContext, schemas, coerceToFullAction(data));
 }
 
 export async function getSystemTemplateCompletion(