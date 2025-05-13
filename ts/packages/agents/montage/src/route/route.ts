4707e78d6fd7193f6d9f61ad73377bacc2cf2728
robgruen <robgruen@microsoft.com>
2025-05-13 14:37:58 -0700

    Fixed montage loading when starting the shell with the canvas visible. (#1100)
    Also creating a new montage will automatically open the shell canvas if
    needed.

--- ts/packages/agents/montage/src/route/route.ts
+++ ts/packages/agents/montage/src/route/route.ts
@@ -194,9 +194,11 @@ app.get("/events", (req, res) => {
     clients.push(res);
 
     // drain any messages in the message queue
-    while (messageQueue.length > 0) {
-        sendDataToClients(messageQueue.shift());
-    }
+    setTimeout(() => {
+        while (messageQueue.length > 0) {
+            sendDataToClients(messageQueue.shift());
+        }
+    }, 100);
 
     req.on("close", () => {
         clients = clients.filter((client) => client !== res);
