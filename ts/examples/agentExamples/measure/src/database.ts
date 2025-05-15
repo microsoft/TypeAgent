f8e1afb95a236c435252b227436f0450c58f2f30
Curtis Man <curtism@microsoft.com>
2025-05-15 11:36:15 -0700

    Reinstate special handler better-sqlite3 to load native module for node

--- ts/examples/agentExamples/measure/src/database.ts
+++ ts/examples/agentExamples/measure/src/database.ts
@@ -2,7 +2,22 @@
 // Licensed under the MIT License.
 
 import Database, * as sqlite from "better-sqlite3";
+import { createRequire } from "node:module";
+import path from "node:path";
 import { removeFile } from "typeagent";
+
+function getDbOptions() {
+    if (process?.versions?.electron !== undefined) {
+        return undefined;
+    }
+    const r = createRequire(import.meta.url);
+    const betterSqlitePath = r.resolve("better-sqlite3/package.json");
+    const nativeBinding = path.join(
+        betterSqlitePath,
+        "../build/Release-Node/better_sqlite3.node",
+    );
+    return { nativeBinding };
+}
 
 export async function createDatabase(
     filePath: string,
@@ -11,7 +26,7 @@ export async function createDatabase(
     if (createNew) {
         await deleteDatabase(filePath);
     }
-    const db = new Database(filePath);
+    const db = new Database(filePath, getDbOptions());
     db.pragma("journal_mode = WAL");
     return db;
 }
