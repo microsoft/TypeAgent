a8fc2c6e607f655f6bc4fdfc2264c02c17634575
Guido van Rossum <gvanrossum@microsoft.com>
2025-06-16 13:10:26 -0700

    Make __main__.py script-able

--- python/ta/typeagent/demo/__main__.py
+++ python/ta/typeagent/demo/__main__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from . import ui
+# Absolute import so we can run this as a script.
+from typeagent.demo import ui
 
 ui.main()
