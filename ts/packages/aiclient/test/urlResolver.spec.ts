0ceb67dbc87d44167641b6edb66b93ba808cbfb4
Robert Gruen <robgruen@microsoft.com>
2025-06-23 09:02:37 -0700

    fixed merge issues

---
+++ ts/packages/aiclient/test/urlResolver.spec.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { testIf } from "./testCore.js";
+import * as urlResolver from "../src/urlResolver.js";
+import { bingWithGrounding } from "../src/index.js";
+
+function hasUrlResolverApiKey() {
+    try {
+        const appSettings = bingWithGrounding.apiSettingsFromEnv()
+        return (
+            appSettings.urlResolutionAgentId !== undefined 
+            && appSettings.urlResolutionAgentId.length > 0
+            && appSettings.validatorAgentId !== undefined
+            && appSettings.validatorAgentId.length > 0
+        );
+    } catch {}
+    return false;
+}
+
+describe("urlResolver", () => {
+    testIf(
+        () => hasUrlResolverApiKey(),
+        "urlResolver.resolveUrl",
+        async () => {
+            const resolved = await urlResolver.resolveURLWithSearch("microsoft", bingWithGrounding.apiSettingsFromEnv());
+
+            expect(resolved).toBe("https://www.microsoft.com/en-us/");
+        }
+    );
+
+    testIf(
+        () => hasUrlResolverApiKey(),
+        "urlResolver.validateUrl",
+        async () => {
+            const validated: urlResolver.urlValidityAction | undefined = await urlResolver.validateURL("microsoft", "https://www.microsoft.com/en-us/", bingWithGrounding.apiSettingsFromEnv());
+
+            expect(validated?.urlValidity).toBe("valid");
+        }
+    );
+
+});
