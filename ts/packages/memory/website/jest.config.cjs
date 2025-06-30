1faf7dd58b202aa340f1bd689e55890121da2157
Hillary Mutisya <150286414+hillary-mutisya@users.noreply.github.com>
2025-06-30 14:09:25 -0700

    lint fix

--- ts/packages/memory/website/jest.config.cjs
+++ ts/packages/memory/website/jest.config.cjs
@@ -7,6 +7,6 @@ module.exports = {
     ...baseConfig,
     testMatch: [
         "**/dist/test/**/*.(spec|test).js?(x)",
-        "!**/dist/test/enhanced-knowledge.spec.js"
-    ]
+        "!**/dist/test/enhanced-knowledge.spec.js",
+    ],
 };
