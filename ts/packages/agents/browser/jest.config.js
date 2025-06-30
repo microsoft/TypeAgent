5c9e0848c809634578678da9267efa64fd4b3b16
Hillary Mutisya <150286414+hillary-mutisya@users.noreply.github.com>
2025-06-30 15:12:34 -0700

    Add mocks for RPC is browser tests
    - Update jest tests to ESM format. Update the preset in jest config
    - Add mocks for new RPC mechanism.

--- ts/packages/agents/browser/jest.config.js
+++ ts/packages/agents/browser/jest.config.js
@@ -2,24 +2,26 @@
 // Licensed under the MIT License.
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: "ts-jest",
+export default {
+    preset: "ts-jest/presets/default-esm",
+    extensionsToTreatAsEsm: [".ts"],
     testEnvironment: "jsdom",
     roots: ["<rootDir>/src/", "<rootDir>/test/"],
     testMatch: ["<rootDir>/test/**/*.test.ts"],
     setupFilesAfterEnv: ["<rootDir>/test/jest-setup.js"],
     transform: {
-        "^.+\\.tsx?$": [
+        "^.+\\.(ts|tsx|js|jsx)$": [
             "ts-jest",
             {
                 tsconfig: "<rootDir>/test/tsconfig.json",
+                useESM: true,
                 diagnostics: {
                     warnOnly: true,
                 },
             },
         ],
     },
-    transformIgnorePatterns: ["/node_modules/(?!jest-chrome).+\\.js$"],
+    transformIgnorePatterns: [],
     collectCoverage: false,
     collectCoverageFrom: [
         "<rootDir>/src/extension/serviceWorker/**/*.ts",
@@ -33,6 +35,14 @@ module.exports = {
         "^common-utils$": "<rootDir>/test/mocks/common-utils.js",
         "../../common/webAgentMessageTypes.mjs":
             "<rootDir>/test/mocks/webAgentMessageTypes.js",
+        "^agent-rpc/channel$": "<rootDir>/test/mocks/agent-rpc-channel.js",
+        "^agent-rpc/rpc$": "<rootDir>/test/mocks/agent-rpc-rpc.js",
+        "../../common/contentScriptRpc/client.mjs":
+            "<rootDir>/test/mocks/contentScriptRpc-client.js",
+        "../../common/contentScriptRpc/types.mjs":
+            "<rootDir>/test/mocks/contentScriptRpc-types.js",
+        "../../common/browserControl.mjs":
+            "<rootDir>/test/mocks/browserControl.js",
         "^src/(.*)$": "<rootDir>/src/$1",
     },
 };
---
+++ ts/packages/agents/browser/test/mocks/browserControl.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Mock for browserControl.mjs
+export const BrowserControlCallFunctions = jest.fn();
+export const BrowserControlInvokeFunctions = jest.fn();
