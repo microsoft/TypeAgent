35c3bccb733946768a27b5b004d9040a27ceb1bf
Umesh Madan <umeshma@microsoft.com>
2025-09-17 13:08:12 -0700

    Related terms

---
+++ dotnet/typeagent/src/knowpro/Term.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace TypeAgent.KnowPro;
+
+public class Term
+{
+    /// <summary>
+    /// The text of the term
+    /// </summary>
+    public string Text { get; set; }
+    /// <summary>
+    /// Optional weighting for the term
+    /// </summary>
+    public float? Weight { get; set; }
+}
+
+public class SearchTerm
+{
+    /// <summary>
+    /// Term being searched for
+    /// </summary>
+    public Term Term { get; set; }
+    /// <summary>
+    ///  Additional terms related to term.
+    /// </summary>
+    public Term[]? RelatedTerms { get; set; }
+}
