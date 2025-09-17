a2985c9c98dc6f17037ab63f56e8383618fad74c
Umesh Madan <umeshma@microsoft.com>
2025-09-17 16:41:08 -0700

    Fixed Visual Studio BOM

--- dotnet/typeagent/src/knowpro/IStorageProvider.cs
+++ dotnet/typeagent/src/knowpro/IStorageProvider.cs
@@ -1,10 +1,10 @@
-﻿// Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 namespace TypeAgent.KnowPro;
 
 public interface IStorageProvider<TMessage>
-    where TMessage: IMessage<IMessageMetadata>
+    where TMessage : IMessage<IMessageMetadata>
 {
     Task<IMessageCollection<TMessage>> GetMessageCollectionAsync();
     Task<ISemanticRefCollection> GetSemanticRefCollection();
--- dotnet/typeagent/src/knowpro/ScoredSemanticRefOrdinal.cs
+++ dotnet/typeagent/src/knowpro/ScoredSemanticRefOrdinal.cs
@@ -1,11 +1,11 @@
-﻿// Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 namespace TypeAgent.KnowPro;
 
 public struct ScoredSemanticRefOrdinal
 {
-    public SemanticRefOrdinal SemanticRefOrdinal{ get; set; }
+    public SemanticRefOrdinal SemanticRefOrdinal { get; set; }
     public float Score { get; set; }
 
     public static ScoredSemanticRefOrdinal New(SemanticRefOrdinal semanticRefOrdinal) { return new ScoredSemanticRefOrdinal { SemanticRefOrdinal = semanticRefOrdinal, Score = 1 }; }
