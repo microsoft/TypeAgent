bfccfb2b920d4ae0769a81be8576adc48ff950f5
Umesh Madan <umeshma@microsoft.com>
2025-09-17 12:36:38 -0700

    Term interfaces

---
+++ dotnet/typeagent/src/knowpro/ITermToSemanticRefIndex.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace TypeAgent.KnowPro;
+
+public interface ITermToSemanticRefIndex
+{
+    Task<int> GetSizeAsync();
+    Task<string[]> GetTermsAsync();
+    Task<string> AddTermAsync(string term, ScoredSemanticRefOrdinal scoredOrdinal);
+}
+
+public static class ITermToSemanticRefIndexEx
+{
+    public static Task<string> AddTermAsync(this ITermToSemanticRefIndex index, string term, SemanticRefOrdinal ordinal)
+    {
+        return index.AddTermAsync(term, new ScoredSemanticRefOrdinal { Score = 1, SemanticRefOrdinal = ordinal });
+    }
+}
---
+++ dotnet/typeagent/src/knowpro/ScoredSemanticRefOrdinal.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace TypeAgent.KnowPro;
+
+public struct ScoredSemanticRefOrdinal
+{
+    public SemanticRefOrdinal SemanticRefOrdinal{ get; set; }
+    public float Score { get; set; }
+}
