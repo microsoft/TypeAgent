8cb0c51398575aab0e10e550856bb7cee2585bd7
Umesh Madan <umeshma@microsoft.com>
2025-09-17 12:22:35 -0700

    Knowledge response

--- dotnet/typeagent/src/knowpro/Facet.cs
+++ dotnet/typeagent/src/knowpro/Facet.cs
@@ -3,16 +3,6 @@
 
 namespace TypeAgent.KnowPro;
 
-public class ConcreteEntity : IKnowledge
-{
-    [JsonPropertyName("name")]
-    public string Name { get; set; }
-    [JsonPropertyName("type")]
-    public string[] Type { get; set; }
-    [JsonPropertyName("facets")]
-    public Facet[] Facets { get; set; }
-}
-
 public class Facet
 {
     public string Name { get; set; }
--- dotnet/typeagent/src/knowpro/Knowledge.cs
+++ dotnet/typeagent/src/knowpro/Knowledge.cs
@@ -26,12 +26,40 @@ public readonly struct KnowledgeType
     }
 }
 
+public class ConcreteEntity : IKnowledge
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("type")]
+    public string[] Type { get; set; }
+    [JsonPropertyName("facets")]
+    public Facet[] Facets { get; set; }
+}
+
 public class Action : IKnowledge
 {
+    [JsonPropertyName("verbs")]
     public string[] Verbs { get; set; }
+    [JsonPropertyName("verbTense")]
+    public string VerbTense { get; set; }
+    [JsonPropertyName("subjectEntityName")]
     public string SubjectEntityName { get; set; }
+    [JsonPropertyName("objectEntityName")]
     public string ObjectEntityName { get; set; }
+    [JsonPropertyName("indirecObjectEntityName")]
     public string IndirectObjectEntityName { get; set; }
+}
+
+public class KnowledgeResponse
+{
+    [JsonPropertyName("entities")]
+    public ConcreteEntity[] Entities { get; set; }
+    [JsonPropertyName("actions")]
+    public Action[] Actions { get; set; }
+    [JsonPropertyName("inverseActions")]
+    public Action[] InverseActions { get; set; }
+    [JsonPropertyName("topics")]
+    public string[] Topic { get; set; }
 }
 
 public class Topic : IKnowledge
