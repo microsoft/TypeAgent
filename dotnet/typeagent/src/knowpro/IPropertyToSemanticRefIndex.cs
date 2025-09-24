1853fa0638592be0f9fd6e89df0655fa7733be60
Umesh Madan <umeshma@microsoft.com>
2025-09-24 13:40:33 -0700

    Entity properties

--- dotnet/typeagent/src/knowpro/IPropertyToSemanticRefIndex.cs
+++ dotnet/typeagent/src/knowpro/IPropertyToSemanticRefIndex.cs
@@ -18,8 +18,71 @@ public interface IPropertyToSemanticRefIndex
 
 public static class PropertyToSemanticRefIndexEx
 {
-    public static Task AddPropertyTermAsync(this IPropertyToSemanticRefIndex index, string propertyName, string value, int semanticRefOrdinal, CancellationToken cancellationToken = default)
+    public static Task AddPropertyAsync(this IPropertyToSemanticRefIndex propertyIndex, string propertyName, string value, int semanticRefOrdinal, CancellationToken cancellationToken = default)
     {
-        return index.AddPropertyAync(propertyName, value, ScoredSemanticRefOrdinal.New(semanticRefOrdinal), cancellationToken);
+        return propertyIndex.AddPropertyAync(propertyName, value, ScoredSemanticRefOrdinal.New(semanticRefOrdinal), cancellationToken);
     }
+
+    public static async Task AddEntityProperties(
+        this IPropertyToSemanticRefIndex propertyIndex,
+        ConcreteEntity entity,
+        int semanticRefOrdinal,
+        CancellationToken cancellationToken = default
+    )
+    {
+        KnowProVerify.ThrowIfInvalid(entity);
+
+        // TODO: Bulk operations
+
+        await propertyIndex.AddPropertyAsync(
+            KnowledgePropertyName.EntityName,
+            entity.Name,
+            semanticRefOrdinal,
+            cancellationToken
+        );
+        foreach (var type in entity.Type)
+        {
+            await propertyIndex.AddPropertyAsync(
+                KnowledgePropertyName.EntityType,
+                type,
+                semanticRefOrdinal,
+                cancellationToken
+            );
+        }
+        // add every facet name as a separate term
+        if (entity.HasFacets)
+        {
+            foreach (var facet in entity.Facets)
+            {
+                await propertyIndex.AddFacetAsync(facet, semanticRefOrdinal, cancellationToken);
+            }
+        }
+
+    }
+
+    public static async Task AddFacetAsync(
+        this IPropertyToSemanticRefIndex propertyIndex,
+        Facet facet,
+        int semanticRefOrdinal,
+        CancellationToken cancellationToken
+    )
+    {
+        // TODO: Bulk operations
+        await propertyIndex.AddPropertyAsync(
+                KnowledgePropertyName.FacetName,
+                facet.Name,
+                semanticRefOrdinal,
+                cancellationToken
+            );
+        if (facet.Value is not null)
+        {
+            await propertyIndex.AddPropertyAsync(
+                KnowledgePropertyName.FacetValue,
+                facet.Value.ToString(),
+                semanticRefOrdinal,
+                cancellationToken
+            );
+        }
+    }
+
 }
