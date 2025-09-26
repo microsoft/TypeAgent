2aa0c9c7f49db1e0b7d02fa983cd4c4a569802ae
Steve Lucco <steveluc@users.noreply.github.com>
2025-09-26 07:46:53 -0700

    fetch music recommendations into track collections (#1659)
    set up to continue playing radio based on played track or context

--- ts/packages/agents/player/src/trackCollections.ts
+++ ts/packages/agents/player/src/trackCollections.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 import { toTrackObjectFull } from "./spotifyUtils.js";
-
 // for now, no paging of track lists; later add offset and count
 export interface ITrackCollection {
     getTrackCount(): number;
@@ -12,11 +11,37 @@ export interface ITrackCollection {
     copy(): ITrackCollection;
 }
 
+function toAlbumObjectSimplified(
+    albumRec: SpotifyApi.RecommendationAlbumObject,
+): SpotifyApi.AlbumObjectSimplified {
+    return {
+        ...albumRec,
+        album_type: "album",
+        href: "",
+        id: albumRec.id,
+        images: [],
+        release_date: "",
+        release_date_precision: "day",
+        total_tracks: 0,
+        uri: albumRec.uri,
+    };
+}
 export class TrackCollection implements ITrackCollection {
     constructor(
         private readonly tracks: SpotifyApi.TrackObjectFull[],
         private readonly contextUri?: string,
     ) {}
+
+    static fromRecommendationTracks(
+        recommendations: SpotifyApi.RecommendationTrackObject[],
+    ) {
+        return new TrackCollection(
+            recommendations.map((track) =>
+                toTrackObjectFull(track, toAlbumObjectSimplified(track.album)),
+            ),
+            undefined,
+        );
+    }
 
     getTracks() {
         return this.tracks;
