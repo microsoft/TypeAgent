4f2b47377fe862782c20cf62514b814591ff34b8
Umesh Madan <umeshma@microsoft.com>
2025-10-13 11:37:38 -0700

    Message api

--- dotnet/typeagent/src/knowpro/IMessageCollection.cs
+++ dotnet/typeagent/src/knowpro/IMessageCollection.cs
@@ -12,3 +12,28 @@ public interface IMessageCollection : IReadOnlyAsyncCollection<IMessage>
 {
 
 }
+
+
+public static class MessageCollectionExtensions
+{
+    /*
+    public ValueTask<int> GetCountOfMessagesInCharBudget(this IMessageCollection messages, IEnumerable<int> messageOrdinals, int maxCharsInBudget)
+    {
+        int i = 0;
+        int totalCharCount = 0;
+
+        foreach (var messageOrdinal in messageOrdinals)
+        {
+            var message = messages.GetAsync(messageOrdinal);
+        const messageCharCount = getMessageCharCount(message);
+            if (messageCharCount + totalCharCount > maxCharsInBudget) {
+                break;
+            }
+            totalCharCount += messageCharCount;
+            ++i;
+        }
+        return i;
+    }
+    */
+
+}
