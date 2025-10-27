3d1a271816adae64d5d484d136fa611ac4d63420
Hillary Mutisya <150286414+hillary-mutisya@users.noreply.github.com>
2025-10-27 14:31:49 -0700

    Add knowledge extraction tests

---
+++ ts/packages/agents/browser/test/mocks/index.ts
@@ -0,0 +1,29 @@
+import { MockLLMResponses } from "./mockLLMResponses";
+import { MockEmbeddings } from "./mockEmbeddings";
+import {
+    MockKnowledgeStore,
+    type Entity,
+    type Relationship,
+    type Topic,
+    type Message,
+} from "./mockKnowledgeStore";
+
+export { MockLLMResponses, MockEmbeddings, MockKnowledgeStore };
+export type { Entity, Relationship, Topic, Message };
+
+export function setupTestMocks() {
+    MockEmbeddings.clearCache();
+
+    const knowledgeStore = new MockKnowledgeStore();
+
+    return {
+        llm: MockLLMResponses,
+        embeddings: MockEmbeddings,
+        knowledgeStore,
+        websiteCollection: knowledgeStore.createMockWebsiteCollection(),
+    };
+}
+
+export function resetAllMocks() {
+    MockEmbeddings.clearCache();
+}
