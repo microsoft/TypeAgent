710b94861e44d1af1e0bd7124ab18a67fd662bae
Robert Gruen <robgruen@microsoft.com>
2025-10-26 18:11:44 -0700

    added id from schema file

--- ts/examples/cacheRESTEndpoint/src/index.ts
+++ ts/examples/cacheRESTEndpoint/src/index.ts
@@ -5,7 +5,8 @@ import express, { Request, Response } from "express";
 import { AgentCache, AgentCacheFactory, MatchResult, RequestAction } from "agent-cache";
 import { existsSync } from "fs";
 import path from "path";
-
+import { NodeType, SchemaParser } from "action-schema";
+import { NameValue } from "typeagent";
 
 // Create an instance of an Express application
 const app = express();
@@ -14,14 +15,17 @@ const app = express();
 const port = `10482`;
 
 // The cache file to serve
-const cacheFile = './data/v5_sample.json';
+const cacheFile = 'data/v5_sample.json';
 
 // the cache
 let agentCache: AgentCache;
 
+// load the schema
+let schemas: NameValue[] = [];
+const schemaFile: string = `../../packages/agents/settings/src/settingsActionSchema.ts`;
+
 // Set up a route for the root URL ('/')
 app.get('/', async (req: Request, res: Response) => {
-    //res.send(`Hello World! ${req} ${res}`);
 
     const s = req.query['s'];
 
@@ -33,9 +37,23 @@ app.get('/', async (req: Request, res: Response) => {
             const hits: RequestAction[] = [];
 
             matches.forEach((construction) => {
+                
+                // add the id to the schema since that's not stored in the cache
+                construction.match.actions.forEach((a) => {
+                    // TODO: actionName should match interface name
+                    const schema = schemas.find((s) => s.name.startsWith(a.action.actionName));
+                    if (schema) {
+                        // TODO: actually parse the schema instead of using regex
+                        const idMatch = schema.value.match(/id:\s*([^;]+);/);
+                        const idValue = idMatch ? idMatch[1].trim().replaceAll("\"", "") : undefined;
+                        (a.action as any).id = idValue;
+                    }
+                });
+                
                 hits.push(construction.match);
-            });
 
+            });
+            
             res.send(`${JSON.stringify(hits, null, 0)}`);
         } else {
             res.status(404).send("{ \"error\": \"No matches found.\" }");
@@ -65,4 +83,27 @@ app.listen(port, async () => {
         process.exit(1);
     }
 
+    if (existsSync(schemaFile)) {
+        schemas = getActionSchema(schemaFile)
+    }
+
 });
+
+function getActionSchema(filePath: string): NameValue[] {
+    const schema = new SchemaParser();
+    schema.loadSchema(filePath);
+    const types = schema.actionTypeNames();
+    const schemas: NameValue[] = [];
+    for (const type of types) {
+        const node = schema.openActionNode(type);
+        let schemaText = node?.symbol.value!;
+        for (const subType of node!.children) {
+            if (subType.symbol.type === NodeType.TypeReference) {
+                schemaText += "\n\n" + subType.symbol.value;
+            }
+        }
+        schemas.push({ name: node?.symbol.name!, value: schemaText });
+    }
+    return schemas;
+}
+
