05c43eba1b481061fc75c7014e59604d044a9009
Robert Gruen <robgruen@microsoft.com>
2025-10-29 11:03:43 -0700

    minor clarification

--- ts/packages/agents/settings/src/settingsActionSchema.ts
+++ ts/packages/agents/settings/src/settingsActionSchema.ts
@@ -5,6 +5,9 @@ export type SettingsAction = DimBrightNessAction;
 
 // An action to dim or brighten the screen
 export interface DimBrightNessAction {
+    // an internal unique identifier for the action
+    id: "settings/dimBrightness";
+    // the name of the action
     actionName: "dimBrightNessAction";
     parameters: {
         // the original request of the user
