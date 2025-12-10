5951534f62e01ec4dab011f78cfd9a2319b4981c
Curtis Man <curtism@microsoft.com>
2025-12-10 09:46:26 -0800

    Agent server client export types

--- ts/packages/dispatcher/rpc/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export * from "@typeagent/dispatcher-types";
-export * from "./clientIOClient.js";
-export * from "./clientIOServer.js";
-export * from "./dispatcherClient.js";
-export * from "./dispatcherServer.js";
