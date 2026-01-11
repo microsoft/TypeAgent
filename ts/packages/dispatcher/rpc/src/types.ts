b689e0e74b90e05f5ba0465b2d84f4b0acfb4ade
Curtis Man <curtism@microsoft.com>
2026-01-10 17:04:52 -0800

    Add type to exports (#1819)

--- ts/packages/dispatcher/rpc/src/types.ts
+++ ts/packages/dispatcher/rpc/src/types.ts
@@ -3,4 +3,10 @@
 
 // Re-export dispatcher-types so dependents don't need to import them directly.
 export type * from "@typeagent/dispatcher-types";
-export type { MessageContent, DisplayType } from "@typeagent/agent-sdk";
+export type {
+    DisplayAppendMode,
+    DisplayContent,
+    MessageContent,
+    DisplayType,
+    DisplayMessageKind,
+} from "@typeagent/agent-sdk";
