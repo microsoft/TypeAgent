8858fa9b61cdc415206dc55f80ec0fa6cc21c693
George Ng <georgeng@microsoft.com>
2026-08-13 23:23:29 -0700

    Remove intentional test failure

--- ts/packages/utils/commonUtils/test/pipelineFailureSummary.spec.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-describe("TEMP build-ts failure summary validation", () => {
-    it("surfaces this intentional failure at the end of the test output", () => {
-        expect("intentional pipeline failure").toBe("remove before merging");
-    });
-});
