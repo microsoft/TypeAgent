71b4f073720eec2e5480321013ed35853a5392b5
George Ng <georgeng@microsoft.com>
2026-08-13 22:18:50 -0700

    Fix the error giving a rachet violation

--- ts/tools/scripts/runTestLocalWithSummary.mjs
+++ ts/tools/scripts/runTestLocalWithSummary.mjs
@@ -80,7 +80,6 @@ function normalizePath(filePath) {
 
 function stripAnsi(value) {
     return value.replace(
-        // eslint-disable-next-line no-control-regex
         /[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[-a-zA-Z\d/#&.:=?%@~_]+)*)?\u0007)|(?:(?:\d{1,4}(?:[;:]\d{0,4})*)?[\dA-PR-TZcf-nq-uy=><~]))/g,
         "",
     );
