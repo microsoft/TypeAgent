7a51c8ba89b2c686ff66a5ebe6572645d2c5ab99
Dominic Nguyen <datnguyen5653@gmail.com>
2026-09-01 23:14:31 -0700

    Revert "feat(dispatcher): expose translation APIs"
    This reverts commit 536df9e3cd0f21cbf0d1cdec94a2875d823b352d.

--- ts/packages/dispatcher/dispatcher/src/internal.ts
+++ ts/packages/dispatcher/dispatcher/src/internal.ts
@@ -59,34 +59,18 @@ export type {
 export type { UserContext } from "./translation/userContext.js";
 export { resolveUserContextFromSchema } from "./translation/userContext.js";
 export { schemaGuidelines } from "./translation/schemaGuidelines.js";
-export {
-    ActionSchemaFileCache,
-    tryGetActionSchema,
-    createSchemaInfoProvider,
-} from "./translation/actionSchemaFileCache.js";
+export { tryGetActionSchema } from "./translation/actionSchemaFileCache.js";
+export { createSchemaInfoProvider } from "./translation/actionSchemaFileCache.js";
 export { getAllActionConfigProvider } from "./context/inlineAgentProvider.js";
 export type { ComposeSchemaOptions } from "./translation/actionSchemaJsonTranslator.js";
-export {
-    convertToActionConfig,
-    type ActionConfig,
-} from "./translation/actionConfig.js";
-export type {
-    ActionConfigProvider,
-    ActionSchemaFile,
-} from "./translation/actionConfigProvider.js";
-export { createHistoryContext } from "./translation/interpretRequest.js";
-export { translateRequest } from "./translation/translateRequest.js";
-export {
-    DispatcherClarifyName,
-    isUnknownAction,
-} from "./context/dispatcher/dispatcherUtils.js";
+export type { ActionConfig } from "./translation/actionConfig.js";
+export type { ActionConfigProvider } from "./translation/actionConfigProvider.js";
 
 export {
     ChatHistoryInput,
     ChatHistoryInputEntry,
     ChatHistoryInputAssistant,
     isChatHistoryInput,
-    createChatHistory,
 } from "./context/chatHistory.js";
 
 export {
@@ -95,9 +79,6 @@ export {
     getSessionNames,
     getSessionConstructionDirPath,
     getSessionConstructionDirPaths,
-    type CollisionStrategy,
-    type DispatcherConfig,
-    Session,
 } from "./context/session.js";
 
 export { initializeGeolocation } from "./context/geolocation.js";
