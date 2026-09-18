211d3507d25bbe8de18d2345594fc4dfa0c616cc
Dominic Nguyen <datnguyen5653@gmail.com>
2026-09-18 14:15:15 -0700

    Split parameter grader into focused modules

--- ts/packages/benchmarks/src/translationBench/synthesizer/catalogGenerator/index.ts
+++ ts/packages/benchmarks/src/translationBench/synthesizer/catalogGenerator/index.ts
@@ -3,4 +3,8 @@
 
 export * from "./paramTypes.js";
 export * from "./schemaTypeConvert.js";
+export * from "./paramGraderNameHeuristics.js";
+export * from "./paramGraderTypes.js";
+export * from "./paramGraderClassify.js";
+export * from "./paramGraderValidate.js";
 export * from "./actionParametersGrader.js";
